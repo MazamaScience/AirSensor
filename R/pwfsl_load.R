49780ff54031519603d2a9664da4b016ed6fb166
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-01-23 08:28:26 -0800

    Minor documentation tweaks:
    We use \code{functionName()} -- with parens -- in the documentation
    to make it easier to identify functions.

--- R/pwfsl_load.R
+++ R/pwfsl_load.R
@@ -5,19 +5,19 @@
 #' 
 #' @description Loads recent PM2.5 monitoring data from the US Forest Service
 #' Pacific Wildland Fire Sciences Lab. This function performs the same data loading 
-#' step as \code{pwfsl_loadLatest}, but has a shorter name for consistency within 
-#' the \code{MazamaPurpleAir} package. By default, this loads data from all 50 states
-#' for the past 10 days. 
+#' step as \code{pwfsl_loadLatest()}, but has a shorter name for consistency with other
+#' data loading functions in the \code{MazamaPurpleAir} package. By default,
+#' this function loads data from all 50 states for the past 10 days. 
 #'
 #' This function is a wrapper around \code{PWFSLSmoke::monitor_loadLatest}.
 #' 
-#' Data for the most recent 45 days can be downloaded using \code{monitor_load}
-#' in the PWFSLSmoke package, whose value will be a \emph{ws_monitor} object with 
-#' the same structuer as the value returned by \code{pwfsl_loadLatest}. 
+#' Data for the most recent 45 days can be downloaded using 
+#' \code{PWFSLSmoke::monitor_loadDaily()}. See the \code{PWFSLSmoke package} for
+#' additional data loading functions.
 #' 
 #' @inheritParams PWFSLSmoke::monitor_loadLatest
 #' 
-#' @return List with \code{meta} and \code{data} elements
+#' @return List with \code{meta} and \code{data} elements.
 #' 
 #' @examples
 #' \dontrun{
--- R/pwfsl_loadLatest.R
+++ R/pwfsl_loadLatest.R
@@ -5,15 +5,15 @@
 #' 
 #' @description Loads recent PM2.5 monitoring data from the US Forest Service
 #' Pacific Wildland Fire Sciences Lab. This function performs the same data loading 
-#' step as \code{pwfsl_load}, but has a different name for naming consistency with the 
-#' \code{PWFSLSmoke} package. By default, this loads data from all 50 states
-#' for the past 10 days. 
+#' step as \code{pwfsl_load()}, but has a longer name for consistency with other 
+#' data loading functions in the \code{PWFSLSmoke} package. By default, 
+#' this function loads data from all 50 states for the past 10 days. 
 #'
 #' This function is a wrapper around \code{PWFSLSmoke::monitor_loadLatest}.
 #' 
-#' Data for the most recent 45 days can be downloaded using \code{monitor_load}
-#' in the PWFSLSmoke package, whose value will be a \emph{ws_monitor} object with 
-#' the same structure as the value returned by \code{pwfsl_loadLatest}. 
+#' Data for the most recent 45 days can be downloaded using 
+#' \code{PWFSLSmoke::monitor_loadDaily()}. See the \code{PWFSLSmoke package} for
+#' additional data loading functions.
 #' 
 #' @inheritParams PWFSLSmoke::monitor_loadLatest
 #' 
@@ -25,8 +25,8 @@
 #' }
 
 pwfsl_loadLatest <- function() {
-  # TODO: change the logger.debug quoted statement to pwfsl_loadLatest as well?
-  logger.debug("----- pwfsl_load() -----")
+
+  logger.debug("----- pwfsl_loadLatest() -----")
   
   # Download PWFSL data
   pwfsl <- PWFSLSmoke::monitor_loadLatest()
