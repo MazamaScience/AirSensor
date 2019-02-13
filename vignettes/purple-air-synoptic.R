34e7b997e412edafed4d6a43e977a08977cd206a
RubyFore <ruby.a.fore@gmail.com>
2019-02-13 12:28:24 -0500

    edits to synoptic vignette

--- vignettes/purple-air-synoptic.R
+++ vignettes/purple-air-synoptic.R
@@ -1,31 +1,28 @@
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(fig.width=7, fig.height=5)
 
-## ----loading-packages-and-data, message=FALSE, warnings='suppress'-------
-# loading required package
+## ----pas_objects---------------------------------------------------------
+# loading MazamaPurpleAir 
 library(MazamaPurpleAir)
+# and MazamaSpatialUtils
 library(MazamaSpatialUtils)
 
-# using pre-loaded data
-data("pas_raw")
+# Loading package data
+data("pas")
 
-# code to download new data 
-# pas_raw <- downloadParseSynopticData()
+# Code to download new data
+# pas <- pas_load()
 
-## ----names---------------------------------------------------------------
-names(pas_raw)
+## ----examining_pas-------------------------------------------------------
+class(pas)
+dim(pas)
 
-## ----names-enhanced------------------------------------------------------
-initializeMazamaSpatialUtils()
-pas_enhanced <- enhanceSynopticData(pas_raw)
-names(pas_enhanced)
-
-## ----interactive-mapping-------------------------------------------------
-pas_leaflet(pas_enhanced)
+## ----column_names--------------------------------------------------------
+colnames(pas)
 
 ## ----subsetting----------------------------------------------------------
 # subsetting using piping and filter() from magrittr and dplyr, respecively
-WestCoastPas <- pas_enhanced %>%
+WestCoastPas <- pas %>%
   filter(stateCode %in% c('OR', 'WA', 'CA'))
 # And plotting to check what the result is 
 pas_leaflet(WestCoastPas)
