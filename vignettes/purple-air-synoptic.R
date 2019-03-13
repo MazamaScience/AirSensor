529e5bedb70095d923c0bd954c1694214974f455
Hans Martin <hansmrtn@gmail.com>
2019-03-13 14:48:42 -0700

    update vignettes with new sample data

--- vignettes/purple-air-synoptic.R
+++ vignettes/purple-air-synoptic.R
@@ -1,28 +1,28 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE, message=FALSE---------------------------------
 knitr::opts_chunk$set(fig.width=7, fig.height=5)
 
-## ----pas_objects---------------------------------------------------------
+## ----pas_objects, message=FALSE------------------------------------------
 # loading MazamaPurpleAir 
 library(MazamaPurpleAir)
 # and MazamaSpatialUtils
 library(MazamaSpatialUtils)
 
 # Loading package data
-data("pas")
+data("pas_load_sample")
 
 # Code to download new data
 # pas <- pas_load()
 
 ## ----examining_pas-------------------------------------------------------
-class(pas)
-dim(pas)
+class(pas_load_sample)
+dim(pas_load_sample)
 
 ## ----column_names--------------------------------------------------------
-colnames(pas)
+colnames(pas_load_sample)
 
 ## ----subsetting----------------------------------------------------------
 # subsetting using piping and filter() from magrittr and dplyr, respecively
-WestCoastPas <- pas %>%
+WestCoastPas <- pas_load_sample %>%
   filter(stateCode %in% c('OR', 'WA', 'CA'))
 # And plotting to check what the result is 
 pas_leaflet(WestCoastPas)
@@ -33,7 +33,7 @@ hist(WestCoastPas$pwfsl_closestDistance)
 ## ----subsetting-2--------------------------------------------------------
 # subset 
 WestCoastPas <- WestCoastPas %>%
-  filter(pwfsl_closestDistance <= 1609)
+  filter(pwfsl_closestDistance <= 16009)
 # and plot 
 pas_leaflet(WestCoastPas)
 
