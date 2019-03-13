440057be2ee0f3159b6a9b2006303c66d2b4911d
Hans Martin <hansmrtn@gmail.com>
2019-03-13 14:37:54 -0700

    use sample data

--- tests/testthat/test-pas_load.R
+++ tests/testthat/test-pas_load.R
@@ -1,8 +1,8 @@
 context("test-pas_load")
 
 test_that("input validation works", {
-  expect_error(pas_load(countryCodes = c(1:4)))
-  expect_error(pas_load(countryCodes = c('US','MX','XX')))
-  expect_error(pas_load(includePWFSL = "X"))
-  expect_error(pas_load(lookbackDays = 0))
+  expect_error(pas_load_sample(countryCodes = c(1:4)))
+  expect_error(pas_load_sample(countryCodes = c('US','MX','XX')))
+  expect_error(pas_load_sample(includePWFSL = "X"))
+  expect_error(pas_load_sample(lookbackDays = 0))
 })
