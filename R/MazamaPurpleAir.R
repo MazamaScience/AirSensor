6db2ac2cfb51823766ba1117c50cbd2a8f97b8ab
Hans Martin <hansmrtn@gmail.com>
2019-03-13 16:09:31 -0700

    add sample documentation

---
+++ R/MazamaPurpleAir.R
@@ -0,0 +1,21 @@
+#' Synoptic Purple Air sensor data, enhanced 
+#' 
+#' Purple Air Sensor data returned by pas_load, with human readable column names. 
+#' 
+#' @import MazamaCoreUtils
+#' @format a dataframe (also a pas_synoptic object) with 500 randomly sampled
+#'  observations of 35 variables. 
+#' 
+#'
+#'
+"pas_load_sample"
+
+#' Synoptic Purple Air Sensor data, pre-enhancement"
+#' 
+#' Purple Air Sensor data returned by downloadParseSynopticData(), with some 
+#' human un-readable column names. 
+#' @import MazamaCoreUtils
+#' @format A dataframe with 11663 observations of 32 variables. 
+#' 
+#' @source https://www.purpleair.com/json
+"raw_pas_sample"
