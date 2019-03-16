1f143e5bfb487643e36ea76def8dbabf3cb2de8b
Hans Martin <hansmrtn@gmail.com>
2019-03-15 23:47:58 -0700

    fixed tests

--- tests/testthat/test-downloadParseSynopticData.R
+++
@@ -1,5 +0,0 @@
-context("test-downloadParseSynopticData")
-
-test_that("input validation check for 'www.purpleair.com' in URL works", {
-  expect_error(downloadParseSynopticData("DUMMY"))
-})
--- tests/testthat/test-enhanceSynopticData.R
+++ tests/testthat/test-enhanceSynopticData.R
@@ -2,14 +2,15 @@ context("test-enhanceSynopticData")
 
 test_that("input validation works", {
   # Load package internal, test 'pas' object
-  data("example_pas")
+  data("example_raw_pas")
   
   # Catch errors first
   expect_error(enhanceSynopticData(c(1:4)))
-  expect_error(enhanceSynopticData(pas_load_sample,
+  expect_error(enhanceSynopticData(example_raw_pas,
                                    countryCodes = c(1:4)))
-  expect_error(enhanceSynopticData(pas_load_sample, 
+  expect_error(enhanceSynopticData(example_raw_pas, 
                                    countryCodes = c('US','MX','XX')))
-  expect_error(enhanceSynopticData(pas_load_sample, 
+  expect_error(enhanceSynopticData(example_raw_pas, 
                                    includePWFSL = "X"))
+  
 })
