be329bdd5636dc9bc75d963faf487ced787cd6ba
hansmrtn@gmail.com <hansmrtn@gmail.com>
2019-04-03 11:36:57 -0700

    fixed pipeing functionality

--- R/pat_multiplot.R
+++ R/pat_multiplot.R
@@ -20,10 +20,10 @@
 #' cookbook-r.com.
 
 pat_multiplot <- function(
-  ..., 
   pat = NULL, 
   plottype = NULL, 
-  cols = NULL
+  cols = NULL, 
+  ...
 ) {
   
   options(warn = -1)
@@ -58,20 +58,20 @@ pat_multiplot <- function(
     
     df <- 
       dplyr::tibble(datetime = pat$data$datetime,
-                    pm25A = pat$data$pm25_A, 
-                    pm25B = pat$data$pm25_B, 
+                    pm25_A = pat$data$pm25_A, 
+                    pm25_B = pat$data$pm25_B, 
                     humidity = pat$data$humidity, 
                     temp = pat$data$temperature)
     
     channelA <- 
       df %>% 
-      ggplot2::ggplot(ggplot2::aes(x = .data$datetime, y = .data$pm25A)) + 
+      ggplot2::ggplot(ggplot2::aes(x = .data$datetime, y = .data$pm25_A)) + 
       ggplot2::geom_point(size = 1, shape = 18, alpha = 1/10) + 
       ggplot2::ggtitle(expression("Channel A PM"[2.5])) + 
       ggplot2::xlab("Date") + ggplot2::ylab("\u03bc g / m\u00b3") 
     channelB <-   
       df %>% 
-      ggplot2::ggplot(ggplot2::aes(x = .data$datetime, y = .data$pm25B)) + 
+      ggplot2::ggplot(ggplot2::aes(x = .data$datetime, y = .data$pm25_B)) + 
       ggplot2::geom_point(size = 1, shape = 18, alpha = 1/10) + 
       ggplot2::ggtitle(expression("Channel B PM"[2.5])) + 
       ggplot2::xlab("Date") + ggplot2::ylab("\u03bc g / m\u00b3") 
