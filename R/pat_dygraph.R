af00d132007a97e887913633db08c6ce066a56de
Hans Martin <hansmrtn@gmail.com>
2019-04-16 16:08:17 -0700

    fixed color choosing

--- R/pat_dygraph.R
+++ R/pat_dygraph.R
@@ -10,6 +10,7 @@
 #'   representing YYYYMMDD[HH])
 #' @param rollPeriod rolling mean to be applied to the data
 #' @param showLegend logical to toggle display of the legend
+#' @param colors string vector of colors to be used for plotting
 #' @description This function creates interactive graphs that will be displayed
 #'   in RStudio's 'Viewer' tab.
 #' @return Initiates the interactive dygraph plot in RStudio's 'Viewer' tab.
@@ -28,11 +29,12 @@ pat_dygraph <- function(pat,
                         ylab = NULL,
                         tlim = NULL,
                         rollPeriod = 1,
-                        showLegend = TRUE) {
+                        showLegend = TRUE,
+                        colors = NULL) {
   
   # Sanity check
   if ( is.null(pat) ) {
-    stop("must provide pat object")
+    stop("Error: must provide pat object")
   }
   
   # Convert tlim to POSIXct
@@ -53,58 +55,67 @@ pat_dygraph <- function(pat,
   
   # Access time
   datetime <- pat$data$datetime
-  
-  
-  if ( is.null(title) )( title <- pat$meta$label )
-  
-  show <- ifelse(showLegend, "always", "never")
+  pm25_A <- pat$data$pm25_A
+  pm25_B <- pat$data$pm25_B
+  temperature <- pat$data$temperature
+  humidity <- pat$data$humidity
+  label <- pat$meta$label
   
   # Create dygraph
-  # TODO: Use custom options provided by user and custom colors (R&B for pm25)
   
-  makeGraph <- function(timeseriesMatrix) {
+  makeGraph <- function(timeseriesMatrix, 
+                        colors) {
     
-    dygraphs::dygraph(timeseriesMatrix, main = title, xlab = xlab, ylab = ylab) %>%
+    if ( is.null(title) )( title <- label )
+    
+    show <- ifelse(showLegend, "always", "never")
+    
+    graph <- 
+      dygraphs::dygraph(timeseriesMatrix, main = title, xlab = xlab, ylab = ylab) %>%
       dygraphs::dyOptions(useDataTimezone = TRUE) %>% # Always show local time
       dygraphs::dyLegend(show = show, width = 250, labelsSeparateLines = TRUE) %>%
       dygraphs::dyRangeSelector(dateWindow = dateWindow) %>%
-      dygraphs::dyRoller(rollPeriod = rollPeriod)
+      dygraphs::dyRoller(rollPeriod = rollPeriod) %>%
+      dygraphs::dyOptions(colors = colors)
+    
+    return(graph)
     
   }
   
-  
   # Create an xts from all data columns except the first which is 'datetime'
-  if ( is.null(plottype) || plottype == "pm25" ) { 
+  
+  if ( is.null(plottype) || tolower(plottype) == "pm25" ) { 
     
-    channelA <- xts::xts(x=pat$data$pm25_A, order.by=datetime, tzone = tzone)
-    channelB <- xts::xts(x=pat$data$pm25_B, order.by=datetime, tzone = tzone)
+    channelA <- xts::xts(x = pm25_A, order.by = datetime, tzone = tzone)
+    channelB <- xts::xts(x = pm25_B, order.by = datetime, tzone = tzone)
     timeseriesMatrix <- cbind(channelA, channelB)
     names(timeseriesMatrix) <- c("Channel A", "Channel B")
     
     if ( is.null(ylab) )( ylab <- "\u03bcg / m\u00b3" )
+    if ( is.null(colors) )( colors <- c("red", "blue")  )
     
-    makeGraph(timeseriesMatrix)
+  } else if ( tolower(plottype) == "humidity" ) {
     
-  } else if ( plottype == "humidity" ) {
-    
-    humidity <- xts::xts(x=pat$data$humidity, order.by=datetime, tzone = tzone)
-    timeseriesMatrix <- cbind(humidity)
-    names(timeseriesMatrix) <- c(paste0(pat$meta$label, "-Humidity"))
+    humidityData <- xts::xts(x = humidity, order.by = datetime, tzone = tzone)
+    timeseriesMatrix <- cbind(humidityData)
+    names(timeseriesMatrix) <- c(paste0(label, "-Humidity"))
     
     if ( is.null(ylab) )( ylab <- "RH%")
     
-    makeGraph(timeseriesMatrix)
+  } else if ( tolower(plottype) == "temperature" || tolower(plottype) == "temp" ) {
     
-  } else if ( plottype == "temperature" || plottype == "temp" ) {
+    temperatureData <- xts::xts(x = temperature, order.by = datetime, tzone = tzone)
+    timeseriesMatrix <- cbind(temperatureData)
+    names(timeseriesMatrix) <- c(paste0(label, "-Temperature"))
     
-    temperature <- xts::xts(x=pat$data$temperature, order.by=datetime, tzone = tzone)
-    timeseriesMatrix <- cbind(temperature)
-    names(timeseriesMatrix) <- c(paste0(pat$meta$label, "-Temperature"))
+    if ( is.null(ylab) )( ylab <- "\u00b0F" )
     
-    if ( is.null(ylab) )( ylab <- "\u00b0F")
+  } else {
     
-    makeGraph(timeseriesMatrix)
+    stop("Error: invalid plottype parameter")  
     
   }
-
+  
+  return( makeGraph(timeseriesMatrix, colors = colors) )
+  
 }
