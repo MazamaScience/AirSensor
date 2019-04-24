57fa9a4d68f2d75c535c8f6ec4df432dcd8ca595
hansmrtn <hansmrtn@gmail.com>
2019-04-24 16:27:22 -0700

    Added themes, issue #45

--- R/pas_esriMap.R
+++ R/pas_esriMap.R
@@ -15,12 +15,15 @@
 #' @param shape symbol to use for points
 #' @param size size of points
 #' @param alpha opacity of points
+#' @param theme base color or palette to be used. 
+#' @param scaleMin default is 0, minimum value to set scale for color gradient
+#' @param scaleMax default is 150, maximum value to set scale for color gradient
 #'   
 #' @return Plots a map loaded from arcGIS REST with points for each monitor.
 #' 
 #' @description Creates an ESRI map of a \emph{pa_synoptic} object.
-#'
-#' #' Available \code{maptype} options include:
+#' 
+#' Available \code{maptype} options include:
 #' \itemize{
 #' \item{natGeo}
 #' \item{worldStreetMap}
@@ -28,9 +31,21 @@
 #' \item{satellite}
 #' \item{deLorme}
 #' }
-#'
+#' 
+#' Available \code{theme} options include sequential and diverging palettes:
+#' 
+#' The sequential palettes names are
+#'  
+#' \code{"Blues" "BuGn" "BuPu" "GnBu" "Greens" "Greys" "Oranges" "OrRd" "PuBu" 
+#' "PuBuGn" "PuRd" "Purples" "RdPu" "Reds" "YlGn" "YlGnBu" "YlOrBr" "YlOrRd"}
+#' 
+#' The diverging palettes are 
+#' 
+#' \code{"BrBG" "PiYG" "PRGn" "PuOr" "RdBu" "RdGy" "RdYlBu" "RdYlGn" "Spectral"}
+#' 
+#' 
 #' Additional base maps are found at:
-#' \url{http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Basemaps/02r3000001mt000000/}
+#' \url{https://developers.arcgis.com/}
 #'
 #' If \code{centerLon}, \code{centerMap} or \code{zoom} are not specified,
 #' appropriate values will be calcualted using data from the
@@ -40,6 +55,12 @@
 #' ca <- pas_load() %>% filter(stateCode == "CA")
 #' pas_esriMap(ca)
 #' }
+#' \dontrun{
+#' pas %>%
+#'   pas_filter(stateCode == "CA" & pm25 > 75) %>%
+#'   pas_esriMap(theme = "Purples", scaleMax = 250)
+#' }
+#' 
 #' @seealso \code{\link{esriMap_plotOnStaticMap}}
 
 pas_esriMap <- function(
@@ -53,7 +74,10 @@ pas_esriMap <- function(
   zoom = NULL,
   shape = 15, 
   size = 4.0, 
-  alpha = 0.8
+  alpha = 0.8, 
+  theme = "Purples", 
+  scaleMin = 0,
+  scaleMax = 150
 ) {
   
   if ( nrow(pas) == 0 ) {
@@ -120,15 +144,51 @@ pas_esriMap <- function(
       crs = sp::CRS("+init=epsg:4326") )
   }
   
+  # ----- Generate Color Palette -----------------------------------------------
+  
+  if ( tolower(theme) == "aqi" ) { 
+    
+    colorFunc <- 
+      leaflet::colorBin( 
+        PWFSLSmoke::AQI$colors, 
+        bins = PWFSLSmoke::AQI$breaks_24, 
+        na.color = "#bbbbbb" 
+      )
+    
+    AQI_color <- c(PWFSLSmoke::AQI$colors, "#bbbbbb")
+    AQI_label <- c(PWFSLSmoke::AQI$names, "Missing")
+
+    colors <- colorFunc(pas$pm25_1hr)
+    
+    colorGradient <- 
+      ggplot2::scale_color_identity(
+        "AQI",
+        labels = AQI_label,
+        breaks = AQI_color,
+        guide = "legend"
+      )
+    
+  
+  } else { 
+    
+    colors <- pas$pm25
+    palette <- 
+      grDevices::colorRampPalette(RColorBrewer::brewer.pal(n = 8, name = theme))
+
+    colorGradient <- 
+      ggplot2::scale_colour_gradientn(
+        "\u03bcg / m\u00b3",
+        colors = palette(8), 
+        limits = c(scaleMin, scaleMax), 
+        oob = scales::squish, 
+        na.value = "grey50"
+      )
+    
+  }
+
+  
   #----- Generate ggobject with mapRaster --------------------------------------
-  colorFunc <- leaflet::colorBin( PWFSLSmoke::AQI$colors, 
-                                  bins = PWFSLSmoke::AQI$breaks_24, 
-                                  na.color = "#bbbbbb" )
-  
-  AQI_color <- c(PWFSLSmoke::AQI$colors, "#bbbbbb")
-  AQI_label <- c(PWFSLSmoke::AQI$names, "Missing")
-  
-  colors <- colorFunc(pas$pm25_1hr)
+
   
   # Suppress "Coordinate system already present." message
   suppressMessages({
@@ -142,14 +202,11 @@ pas_esriMap <- function(
           color = colors),
         shape = shape, 
         size = size, 
-        alpha = alpha) + 
-      ggplot2::scale_color_identity("AQI", 
-                                    labels = AQI_label, 
-                                    breaks = AQI_color,
-                                    guide = "legend") +
-      ggplot2::coord_fixed(ratio = 4/3) +   # TODO:  comment on purpose
-      ggplot2::theme_void()
-    
+        alpha = alpha) +   
+        ggplot2::coord_fixed(ratio = 4/3) +   # TODO:  comment on purpose
+        ggplot2::theme_void() + 
+        colorGradient 
+        
   })
   
   return(ggRasterPlot)
