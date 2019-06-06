eb02ca50adb9e576e0db698c11025bad3b0093c4
hansmrtn <hansmrtn@gmail.com>
2019-06-06 15:41:13 -0700

    updated dailyAverages issue #57

--- ShinyAirSensor/dailyAveragePlot/app.R
+++ ShinyAirSensor/dailyAveragePlot/app.R
@@ -1,48 +1,178 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This Shiny app demonstrates the use of AST Models to display daily averages
+# - Mazama Science
 
-library(shiny)
+# ---- Debug 
+pas <- example_pas[which(stringr::str_detect(example_pas$label, "SCNP")),] 
+pas_labels <- 
+    pas$label[-which(stringr::str_detect(pas$label, " B"))]
+# ----
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+# Define UI for application
+ui <- shiny::fluidPage(
+    shiny::fluidRow(
+        column(
+            width = 2,
+            
+            
+            # PAS selection input
+            shiny::selectInput(
+                inputId = "pas_select", 
+                label = "Purple Air Sensors:",
+                choices = pas_labels
+            ),
+            
+            shiny::dateRangeInput(
+                inputId = "date_range", 
+                label = "Date Range:"
+            )
+            
+        ), 
+        
+        column(
+            width = 10,
+            # Plot outputs
+            shiny::plotOutput(outputId = "pm25_plot"), 
+            shiny::plotOutput(outputId = "temp_plot"), 
+            shiny::plotOutput(outputId = "hum_plot") 
+            
         )
     )
 )
-
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    
+    output$pm25_plot <- 
+        shiny::renderPlot({
+            pat <- 
+                AirSensor::pat_load(
+                    label = input$pas_select, 
+                    startdate = input$date_range[1], 
+                    enddate = input$date_range[2]
+                ) 
+            ast <- 
+                AirSensor::pat_createASTimeseries(
+                    pat = pat, 
+                    period = "1 day" 
+                )
+            
+            pm25_AB_avg <- 
+                ast$data %>% 
+                dplyr::select(
+                    .data$pm25_A_mean, 
+                    .data$pm25_B_mean
+                ) %>% 
+                dplyr::transmute(
+                    pm25_AB_avg = 
+                        (.data$pm25_A_mean + .data$pm25_B_mean) / 2
+                )
+            
+            pm25_plot <- 
+                ast$data %>% 
+                ggplot2::ggplot(
+                    ggplot2::aes(
+                        x = .data$datetime, 
+                        y = pm25_AB_avg
+                    )
+                ) + 
+                ggplot2::ggtitle(
+                    label = "PM2.5"
+                ) + 
+                ggplot2::xlab("Datetime") + 
+                ggplot2::ylab("\u03bcg / m\u00b3")
+            
+            pm25_avg_bar <- 
+                ggplot2::geom_bar(                   
+                    data = ast$data,
+                    mapping = ggplot2::aes(
+                        x = .data$datetime, 
+                        y = pm25_AB_avg[,1]
+                    ), 
+                    stat = "identity"
+                )
+            
+            return(pm25_plot + pm25_avg_bar)
+            
+        })
+    
+    output$hum_plot <- 
+        shiny::renderPlot({
+            pat <- 
+                AirSensor::pat_load(
+                    label = input$pas_select, 
+                    startdate = input$date_range[1], 
+                    enddate = input$date_range[2]
+                ) 
+            ast <- 
+                AirSensor::pat_createASTimeseries(
+                    pat = pat, 
+                    period = "1 day"
+                )
+            
+            humidity_plot <- 
+                ast$data %>% 
+                ggplot2::ggplot(
+                    ggplot2::aes(
+                        x = .data$datetime, 
+                        y = .data$humidity_mean
+                    )
+                ) + 
+                ggplot2::ggtitle(
+                    label = "Humidity"
+                ) + 
+                ggplot2::xlab("Datetime") + 
+                ggplot2::ylab("RH%") +
+                ggplot2::geom_bar(                   
+                    data = ast$data,
+                    mapping = ggplot2::aes(
+                        x = .data$datetime, 
+                        y = .data$humidity_mean
+                    ), 
+                    stat = "identity"
+                )
+            
+            return(humidity_plot)
+            
+        })
+    
+    output$temp_plot <- 
+        shiny::renderPlot({
+            pat <- 
+                AirSensor::pat_load(
+                    label = input$pas_select, 
+                    startdate = input$date_range[1], 
+                    enddate = input$date_range[2]
+                ) 
+            ast <- 
+                AirSensor::pat_createASTimeseries(
+                    pat = pat, 
+                    period = "1 day"
+                )
+            
+            temperature_plot <- 
+                ast$data %>% 
+                ggplot2::ggplot(
+                    ggplot2::aes(
+                        x = .data$datetime, 
+                        y = .data$temperature_mean
+                    )
+                ) + 
+                ggplot2::ggtitle(
+                    label = "Temperature"
+                ) + 
+                ggplot2::xlab("Datetime") + 
+                ggplot2::ylab("\u00b0F") +
+                ggplot2::geom_bar(                   
+                    data = ast$data,
+                    mapping = ggplot2::aes(
+                        x = .data$datetime, 
+                        y = .data$temperature_mean
+                    ), 
+                    stat = "identity"
+                )
+            
+            return(temperature_plot)
+            
+        })
 }
 
 # Run the application 
