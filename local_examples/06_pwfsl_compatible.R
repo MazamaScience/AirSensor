e8e36cc59097a3f4e01715cfdebf94ae9fb6b729
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-06-11 12:14:01 -0700

    vignette typo

--- local_examples/06_pwfsl_compatible.R
+++ local_examples/06_pwfsl_compatible.R
@@ -2,9 +2,16 @@
 library(AirSensor)
 library(PWFSLSmoke)
 
-ws_monitor <-
+ws_monitor_A <-
   example_pat %>%
   pat_createASTimeseries() %>%
-  ast_createAirSensor()
+  ast_createAirSensor("pm25_A_mean")
 
-monitor_dailyBarplot(ws_monitor)
+ws_monitor_B <-
+  example_pat %>%
+  pat_createASTimeseries() %>%
+  ast_createAirSensor("pm25_B_mean")
+
+layout(matrix(1:2))
+monitor_dailyBarplot(ws_monitor_A)
+monitor_dailyBarplot(ws_monitor_B)
