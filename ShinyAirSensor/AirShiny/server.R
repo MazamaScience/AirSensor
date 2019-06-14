02093776b09a90d7fd58a265cb9953a65549fea4
hansmrtn <hansmrtn@gmail.com>
2019-06-14 15:14:24 -0700

    added Data Explorer and Download, issue #80

--- ShinyAirSensor/AirShiny/server.R
+++ ShinyAirSensor/AirShiny/server.R
@@ -17,89 +17,151 @@ pas_community <- unique(AirSensor::example_pas$communityRegion) %>% na.omit()
 # Define server logic 
 shiny::shinyServer(
     function(input, output, session) {
-        
-        # Leaflet render
-        output$leaflet <- 
-            leaflet::renderLeaflet({
-                pas_in_comm <- 
-                    pas[which(
-                        stringr::str_detect(
-                            pas$communityRegion, 
-                            input$comm_select)
-                    ),]
-                
-                AirSensor::pas_leaflet_shiny(
-                    pas_in_comm, 
-                    parameter = "pm25_1day", 
-                    paletteName = "Spectral")
-            })
-        
-        # Update Selected pas based on leaflet selection
-        shiny::observe({
+    
+    # Leaflet render
+    output$leaflet <- 
+        leaflet::renderLeaflet({
             
             pas_in_comm <- 
                 pas[which(
                     stringr::str_detect(
                         pas$communityRegion, 
-                        input$comm_select
-                    )
+                        input$comm_select)
                 ),]
             
-            shiny::updateSelectInput(
-                session, 
-                inputId = "pas_select", 
-                selected = input$leaflet_marker_click[1], 
-                choices = pas_in_comm$label
+            AirSensor::pas_leaflet_shiny(
+                pas_in_comm, 
+                parameter = "pm25_1day", 
+                paletteName = "Spectral"
             )
             
         })
+    
+    # Update Selected pas based on leaflet selection
+    shiny::observe({
         
-        # # Text debug output 
-        #  output$test <-
-        #      shiny::renderText(paste0(input$leaflet_marker_click))
+        pas_in_comm <- 
+            pas[which(
+                stringr::str_detect(
+                    pas$communityRegion, 
+                    input$comm_select
+                )
+            ),]
         
-        # Standard Plot output   
-        fplot <-
-            shiny::renderPlot({
+        shiny::updateSelectInput(
+            session, 
+            inputId = "pas_select", 
+            selected = input$leaflet_marker_click[1], 
+            choices = pas_in_comm$label
+        )
+        
+    })
+    
+    # # Text debug output 
+    #  output$test <-
+    #      shiny::renderText(paste0(input$leaflet_marker_click))
+    
+    # Standard Plot output   
+    output$selected_plot <-
+        shiny::renderPlot({
+            
+            # NOTE: The current method is not filtering ANY outliers for
+            # NOTE: ANY of the plots - may be prone to change.
+            pat <- 
+                AirSensor::pat_load(
+                    label = input$leaflet_marker_click[1], 
+                    startdate = input$date_range[1], 
+                    enddate = input$date_range[2]
+                )
+            
+            if ( input$plot_type_select == "daily_plot" ) {
                 
-                # NOTE: The current method is not filtering ANY outliers for
-                # NOTE: ANY of the plots - may be prone to change.
-                pat <- 
+                return(AirSensor::shiny_barplot(pat, period = "1 day"))
+                
+            } else if ( input$plot_type_select == "multi_plot" ) { 
+                
+                return(AirSensor::pat_multiplot(pat))
+                
+            }  else if ( input$plot_type_select == "hourly_plot" ) {
+                
+                return(AirSensor::shiny_barplot(pat, period = "1 hour"))
+            }
+            
+        })
+    
+    # TODO: HANDLE SPECIAL DYGRAPH CASE
+    
+    # Data Table
+    output$data_explorer <- 
+        shiny::renderDataTable({
+            
+            pat <- 
+                AirSensor::pat_load(
+                    label = input$leaflet_marker_click[1], 
+                    startdate = input$date_range[1], 
+                    enddate = input$date_range[2]
+                )
+            
+            data <- pat$data 
+            
+            return(data)
+            
+        })
+    
+    # Meta Table
+    output$meta_explorer <- 
+        shiny::renderTable({
+            
+            pat <- 
+                AirSensor::pat_load(
+                    label = input$leaflet_marker_click[1], 
+                    startdate = input$date_range[1], 
+                    enddate = input$date_range[2]
+                )
+            
+            meta <-
+                pat$meta %>% 
+                dplyr::select(
+                    "Sensor Label" = .data$label,
+                    "Sensor Type" = .data$sensorType,
+                    "Longitude" = .data$longitude, 
+                    "Latitude" = .data$latitude,
+                    "State" = .data$stateCode, 
+                    "Country" = .data$countryCode, 
+                    "Timezone" = .data$timezone
+                )
+            
+            return(meta)
+            
+        })
+    
+    # Download button
+    # TODO: FIX RENAMING SCHEME ON PAS SELECTION (REACTIVE?)
+    output$download_data <- 
+        shiny::downloadHandler(
+            filename = paste0(
+                input$leaflet_marker_click[1],
+                "_",
+                input$date_range[1],
+                "_",
+                input$date_range[2], 
+                ".csv"
+            ), 
+            
+            content = function(file) {
+                write.csv(
                     AirSensor::pat_load(
                         label = input$leaflet_marker_click[1], 
                         startdate = input$date_range[1], 
                         enddate = input$date_range[2]
-                    )
+                    )$data,
+                    file = file
+                )
                 
-                if ( input$plot_type_select == "daily_plot") {
-                    
-                    return(AirSensor::shiny_barplot(pat, period = "1 day"))
-                    
-                } else if ( input$plot_type_select == "multi_plot" ) { 
-                    
-                    return(AirSensor::pat_multiplot(pat))
-                    
-                }  else if ( input$plot_type_select == "hourly_plot") {
-                    
-                    return(AirSensor::shiny_barplot(pat, period = "1 hour"))
-                }
-            })
-        
-        # Dygraph (JS) output
-        fdygraph <- 
-            dygraphs::renderDygraph({
-                pat <- 
-                    AirSensor::pat_load(
-                        label = input$leaflet_marker_click[1], 
-                        startdate = input$date_range[1], 
-                        enddate = input$date_range[2]
-                    ) 
-                AirSensor::pat_dygraph(pat) # HANDLE SPECIAL CASE
-            })
-        
-        return(output$selected_plot <- fplot) 
-        
-        # TODO: HANDLE SPECIAL DYGRAPH CASE
-        
+            }
+            
+        )
+    
     }
+
 )
--- ShinyAirSensor/AirShiny/ui.R
+++ ShinyAirSensor/AirShiny/ui.R
@@ -17,70 +17,107 @@ shiny::shinyUI(
     shiny::navbarPage(
         
         # ----- Nav Bar --------------------------------------------------------
-        title = "AirShiny (Beta)",
-        shiny::tabPanel("Interactive Map"),
-        shiny::tabPanel("Data Explorer"),
+        title = "AirShiny (Beta)", 
         
-        shiny::fluidRow(
+        ## ---- Tab 1 ----------------------------------------------------------
+        shiny::tabPanel(
             
-        #### ----- L Column  ---------------------------------------------------
-            shiny::column(
+            title = "Interactive Map",
+            
+            shiny::fluidRow(
                 
-                width = 2,
-                
-                # Community Selection input
-                shiny::selectInput(
-                    inputId = "comm_select", 
-                    label = "Community Selection", 
-                    choices = pas_community 
+                #### ----- L Column  -------------------------------------------
+                shiny::column(
+                    
+                    width = 2,
+                    
+                    # Community Selection input
+                    shiny::selectInput(
+                        inputId = "comm_select", 
+                        label = "Community Selection", 
+                        choices = pas_community 
+                    ), 
+                    
+                    # PAS selection input
+                    shiny::selectInput(
+                        inputId = "pas_select",
+                        label = "Purple Air Sensor:", 
+                        choices = ""
+                    ),
+                    
+                    # Date Range input
+                    shiny::dateRangeInput(
+                        inputId = "date_range", 
+                        label = "Date Range:", 
+                        min = "2017-01-01", 
+                        start = "2019-04-01", # START DEFAULT DATE
+                        end = "2019-04-03" # END 
+                    ),
+                    
+                    # Plot type selection
+                    shiny::selectInput(
+                        inputId = "plot_type_select", 
+                        label = "Plot Selection:", 
+                        choices = c("Hourly Average" = "hourly_plot",
+                                    "Daily Average" = "daily_plot",
+                                    "Multi-sensor Raw Data" = "multi_plot")
+                    )
+                    
                 ), 
                 
-                # PAS selection input
-                shiny::selectInput(
-                    inputId = "pas_select",
-                    label = "Purple Air Sensor:", 
-                    choices = ""
-                ),
-               # Date Range input
-                shiny::dateRangeInput(
-                    inputId = "date_range", 
-                    label = "Date Range:", 
-                    min = "2017-01-01", 
-                    start = "2019-04-01", # START DEFAULT DATE
-                    end = "2019-04-03" # END 
-                ),
-
-                # Plot type selection
-                shiny::selectInput(
-                    inputId = "plot_type_select", 
-                    label = "Plot Selection:", 
-                    choices = c("Hourly Average" = "hourly_plot",
-                                "Daily Average" = "daily_plot",
-                                "Multi-sensor Raw Data" = "multi_plot")
+                ####----- R Column ---------------------------------------------
+                shiny::column(
+                    
+                    width = 10,
+                    
+                    # Plot outputs
+                    leaflet::leafletOutput(
+                        outputId = "leaflet", height = 500
+                    ), 
+                    
+                    # # Debug text
+                    # shiny::textOutput("test"),
+                    
+                    # Selected Plot
+                    shiny::plotOutput(
+                        outputId = "selected_plot"
+                    )
+                    
                 )
-                
-            ), 
-        
-        ####----- R Column -------------------------------------------------
-            shiny::column(
-                
-                width = 10,
-                
-                # Plot outputs
-                leaflet::leafletOutput(
-                    outputId = "leaflet", height = 500
-                ), 
-                
-                # # Debug text
-                # shiny::textOutput("test"),
-                
-                # Selected Plot
-                shiny::plotOutput(outputId = "selected_plot")
                 
             )
             
+        ),
+        
+        ## ----- Tab 2 ---------------------------------------------------------
+        
+        shiny::tabPanel(
+            
+            title = "Data Explorer",
+            
+            # Meta explorer
+            shiny::column( 
+                width = 11,
+                shiny::tableOutput(
+                    outputId = "meta_explorer"
+                )
+            ),
+            
+            # Download Button
+            shiny::column( 
+                width = 1,
+                shiny::downloadButton(
+                    outputId = "download_data"
+                )
+            ),
+            
+            # Data explorer
+            shiny::dataTableOutput(
+                outputId = "data_explorer"
+            )
+            
         )
-    
+        
     )
-
+    
 )
