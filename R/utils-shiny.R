0894a76c3cdc17c89cd84ef6a9d9afb78757bb8d
hansmrtn <hansmrtn@gmail.com>
2019-06-17 12:51:29 -0700

    close issue #77

--- R/utils-shiny.R
+++ R/utils-shiny.R
@@ -6,27 +6,6 @@
 #' @description This function creates interactive maps that will be displayed in 
 #' AirShiny web-app. 
 #'
-#' Typical usage would be to use the \code{parameter} argument to display pm25 
-#' values from one of:
-#' \itemize{
-#' \item{"pm25_current"}
-#' \item{"pm25_10min"}
-#' \item{"pm25_30min"}
-#' \item{"pm25_1hr"}
-#' \item{"pm25_6hr"}
-#' \item{"pm25_1day"}
-#' \item{"pm25_1week"}
-#' }
-#' 
-#' Auxillary \code{parameter} arguments can be usd to display various Purple Air 
-#' sensor data. Currently supported \code{parameter} arguments include:
-#' \itemize{
-#' \item{"humidity"}
-#' \item{"pressure"}
-#' \item{"temperature"}
-#' \item{"pwfsl_closestDistance"}
-#' } 
-#' 
 #' @details The \code{maptype} argument is mapped onto leaflet "ProviderTile" 
 #' names. Current mappings include:
 #' \enumerate{
@@ -47,7 +26,6 @@
 #' @param radius Radius (pixels) of monitor circles.
 #' @param opacity Opacity of monitor circles.
 #' @param maptype Optional name of leaflet ProviderTiles to use, e.g. \code{terrain}. 
-#' @param outsideOnly Logical specifying subsetting for monitors marked as 'outside'.
 #' 
 #' @return A leaflet "plot" object which, if not assigned, is rendered in 
 #' Rstudio's 'Viewer' tab.
@@ -59,12 +37,8 @@ pas_leaflet_shiny <- function(
   paletteName = NULL,
   radius = 11,
   opacity = 0.8,
-  maptype = "terrain",
-  outsideOnly = TRUE
+  maptype = "terrain"
 ) {
-  
-  # TODO: Remove unecessary paramters, etc for the AirShiny version. 
-  #       This will likely be a majority. Consider full re-write
   
   # ----- Validate parameters --------------------------------------------------
   
@@ -74,202 +48,32 @@ pas_leaflet_shiny <- function(
   if ( nrow(pas) == 0 || ncol(pas) == 0 )
     stop(paste0("One or both dimensions of the pa_synoptic object has length 0."))
   
-  if ( !(parameter %in% c('pm25_current', 
-                          'pm25_10min', 
-                          'pm25_30min', 
-                          'pm25_1hr',
-                          'pm25_6hr', 
-                          'pm25_1day', 
-                          'pm25_1week', 
-                          'humidity',
-                          'pressure', 
-                          'temperature', 
-                          'pwfsl_closestDistance')) ) 
-    stop("Required parameter 'parameter' is invalid.")
-  
-  if ( !is.logical(outsideOnly) )
-    stop(paste0('outsideOnly parameter should be TRUE/FALSE'))
-  
   if ( !is.numeric(radius) )
     stop(paste0('radius parameter is non-numeric'))
   
+  colorInfo <- pas_palette(
+    pas, 
+    paletteName = paletteName, 
+    parameter = parameter, 
+    reverse=TRUE
+  )
   
-  # ----- outsideOnly subsetting -----------------------------------------------
+  cols <- colorInfo$colors
   
-  if ( outsideOnly ) {
-    pas <- subset(pas, pas$DEVICE_LOCATIONTYPE == 'outside')
-  }
-  
-  # ----- Figure out names for a legend and colors for each point --------------
-  
-  # Ignore warnings from RColorBrewer as leaflet::colorBin does the right thing
-  suppressWarnings({
-    
-    if ( stringr::str_detect(tolower(parameter), "^pm25") ) { # AQI
-      
-      if ( tolower(paletteName) == "aqi" || is.null(paletteName) ) {
-        
-        colorInfo <- pas_palette(pas, "aqi", parameter)
-        cols <- colorInfo$colors
-        
-        colors <- colorInfo$key[,2]
-        labels <- colorInfo$key[,1]
-        legendTitle <- 'AQI'
-        value <- round(pas$pm25_1hr, 1)
-        unit <- '\U00B5g/m3'
-        
-      } else { # Other Palettes
-        
-        colorInfo <- pas_palette(
-          pas, 
-          paletteName = paletteName, 
-          parameter = parameter, 
-          reverse=TRUE
-        )
-        
-        cols <- colorInfo$colors
-        
-        colors <- colorInfo$key[,2]
-        labels <- c(
-          "0-12", 
-          "12-35", 
-          "35-55", 
-          "55-150", 
-          "150-250", 
-          ">250"
-        )
-        
-        legendTitle <- '\U00B5g/m^3'
-        value <- round(pas[[parameter]], 2)
-        unit <- '\U00B5g/m3'
-      }
-      
-      
-      
-    } else if ( parameter == "temperature" ) {       # Temperature
-      
-      colorInfo <- 
-        pas_palette(
-          pas, 
-          "temperature", 
-          reverse = TRUE
-        )
-      
-      cols <- colorInfo$colors
-      labels <- colorInfo$key[,1]
-      colors <- colorInfo$key[,2]
-      
-      legendTitle <- 'Temp in \U2109'
-      value <- round(pas[[parameter]], 0)
-      unit <- '\U2109'
-      
-    } else if ( parameter == "humidity" ) {          # Humidity
-      
-      colorInfo <- 
-        pas_palette(
-          pas, 
-          "humidity", 
-          reverse = FALSE
-        )
-      
-      cols <- colorInfo$colors
-      labels <- colorInfo$key[,1]
-      colors <- colorInfo$key[,2]
-      
-      value <- round(pas[[parameter]], 0)
-      legendTitle <- 'Relative Humidity'
-      unit <- '%'
-      
-    } else if ( stringr::str_detect(parameter, "[dD]istance$") ) { # Distance
-      
-      colorInfo <- 
-        pas_palette(
-          pas, 
-          "distance", 
-          reverse = TRUE
-        )
-      
-      cols <- colorInfo$colors
-      labels <- colorInfo$key[,1]
-      colors <- colorInfo$key[,2]
-      
-      
-      legendTitle <- parameter
-      value <- round(pas[[parameter]], 0)
-      unit <- 'm'
-      
-    } else if ( parameter %in% c("r2_a", "r2_b") ) {
-      
-      bins <- c(0, .5, .75, .9, 1)
-      domain <- c(0,1)
-      colors <- c("#ffffd4","#fed98e","#fe9929","#cc4c02")
-      colorFunc <- leaflet::colorBin(colors, domain = domain, 
-                                     bins = bins, na.color = "#bbbbbb")
-      cols <- colorFunc(pas[[parameter]])
-      labels <- c('0-.5', '.5-.75', '.75-.9', '.9-1')
-      legendTitle <- "R2"
-      value <- round(pas[[parameter]], 2)
-      unit <- ''
-      
-    } else if ( parameter %in% c("slope_a", "slope_b") ) {
-      
-      colors <- c("#7f3b08", 
-                  "#b35806",
-                  "#e08214",
-                  "#fdb863",
-                  "#fee0b6",
-                  "#d8daeb",
-                  "#b2abd2",
-                  "#8073ac",
-                  "#542788",
-                  "#2d004b")
-      colorFunc <- leaflet::colorBin(colors, 
-                                     domain = range(pas[[parameter]]), 
-                                     bins = 10, 
-                                     na.color = "#bbbbbb")
-      cols <- colorFunc(pas[[parameter]])
-      breaks <- seq(domain[1],domain[2],length.out=11)
-      offset <- diff(breaks)[1] / 2
-      levels <- signif(seq(breaks[1]+offset, breaks[11]-offset,length.out=10), digits=2)
-      labels <- as.character(levels)
-      legendTitle <- "Scale Factor"
-      value <- round(pas[[parameter]], 2)
-      unit <- ''
-      
-    } else {                                    # Other
-      
-      domain <- range(pas[[parameter]], na.rm=TRUE)
-      colorFunc <- leaflet::colorNumeric("Purples", 
-                                         domain = domain, 
-                                         na.color = "#bbbbbb", 
-                                         reverse=FALSE)
-      cols <- colorFunc(pas[[parameter]])
-      breaks <- seq(domain[1],domain[2],length.out=6)
-      offset <- diff(breaks)[1] / 2
-      levels <- signif(seq(breaks[1]+offset, breaks[6]-offset,length.out=5), digits=4)
-      colors <- leaflet::colorBin("Purples", domain=range(breaks), bins=breaks, reverse=FALSE)(levels)
-      labels <- as.character(levels)
-      value <- signif(pas[[parameter]], 4)
-      legendTitle <- parameter
-      unit <- ''
-    }
-    
-    # Create popupText
-    pas$popupText <- paste0(
+  # Create popupText
+  pas$popupText <- 
+    paste0(
       "<b>", pas$label, "</b><br/>",
-      "<b>", parameter, " = ", value, " ", unit, "</b><br/>",
-      "temperature = ", round(pas$temperature, 0), " F<br/>",
-      "humidity = ", round(pas$humidity, 0), "%<br/>",
-      "pm25_1day = ", round(pas$pm25_1day, 1), " \U00B5g/m3<br/>",
-      "location_type = ", pas$DEVICE_LOCATIONTYPE, "<br/"
+      "PM2.5 (Daily) = ", round(pas$pm25_1day, 1), " \U00B5g/m3<br/>",
+      "Temperature = ", round(pas$temperature, 0), " F<br/>",
+      "Humidity = ", round(pas$humidity, 0), "%<br/>"
     )
-    
-  })
   
   # Extract view information
   lonRange <- range(pas$longitude, na.rm = TRUE)
   latRange <- range(pas$latitude, na.rm = TRUE)
-  maxRange <- max(diff(lonRange),diff(latRange), na.rm = TRUE)
+  maxRange <- max(diff(lonRange), diff(latRange), na.rm = TRUE)
+  
   # Determine appropriate zoom level
   if (maxRange > 20) {
     zoom <- 4
@@ -293,8 +97,16 @@ pas_leaflet_shiny <- function(
   
   # Convert locations to SpatialPointsDataFrame
   pas <- pas[!is.na(pas$latitude),]
-  SPDF <- sp::SpatialPointsDataFrame(coords=cbind(pas$longitude,pas$latitude),
-                                     data=as.data.frame(pas))
+  
+  pas <- pas[which(!stringr::str_detect(pas$label, " B")),]
+  SPDF <- 
+    sp::SpatialPointsDataFrame(
+      data = as.data.frame(pas),
+      coords = cbind(
+        pas$longitude, 
+        pas$latitude
+      )
+    )
   
   # Convert maptype to a character string that addProviderTiles can read
   if ( missing(maptype) || maptype == 'terrain') {
@@ -310,23 +122,31 @@ pas_leaflet_shiny <- function(
   }
   
   # Create leaflet map
-  m <- 
-    leaflet::leaflet(SPDF) %>%
+  map <- 
+    leaflet::leaflet(SPDF)
+  
+  map <-  
     leaflet::setView(
+      map, 
       lng=mean(lonRange), 
-      lat=mean(latRange), zoom=zoom
-    ) %>%
-    leaflet::addProviderTiles(providerTiles) %>%
+      lat=mean(latRange),
+      zoom=zoom
+    )
+  map <-
+    leaflet::addProviderTiles(map, providerTiles)
+  
+  map <-
     leaflet::addCircleMarkers(
+      map,
       radius=radius,
       fillColor=cols,
       fillOpacity=opacity,
-      stroke=TRUE,
+      stroke=FALSE,
       popup=pas$popupText,
       layerId = pas$label
     )
   
-  return(m)
+  return(map)
   
 }
 
@@ -381,10 +201,11 @@ shiny_barplot <-
           x = .data$datetime, 
           y = pm25_AB_avg[,1],
           fill = pm25_AB_avg[,1]), 
-        stat = "identity", 
+        stat = "identity",
+        color = "gray", 
         show.legend = FALSE
       )
     
     return(pm25_plot + pm25_avg_bar)
     
-}
+  }
