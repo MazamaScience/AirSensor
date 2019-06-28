c3a2e8035a0667d118265b446f17a0a1b50750b7
Tate Brasel <tate@Shuksan.domain>
2019-06-28 13:54:17 -0700

    Added retries to pas_load()

--- R/pas_load.R
+++ R/pas_load.R
@@ -10,8 +10,9 @@
 #'   and provide a record of all currently installed Purple Air sensors for the
 #'   day of interest.
 #' 
-#'   The \code{datestamp} can be anything that is understood by \code{lubrdiate::ymd()}
-#'   including either of the following recommended formats:
+#'   The \code{datestamp} can be anything that is understood by 
+#'   \code{lubrdiate::ymd()} including either of the following recommended 
+#'   formats:
 #' 
 #' \itemize{
 #' \item{\code{"YYYYmmdd"}}
@@ -20,10 +21,12 @@
 #' 
 #' By default, today's date is used.
 #'
-#' @param datestamp A date string in ymd order.
-#' @param baseUrl The base URL for synoptic data.
+#' @param datestamp Date string in ymd order.
+#' @param baseUrl Base URL for synoptic data.
+#' @param retries Max number of days to go back and try to load if requested 
+#'   date cannot be retrieved.
 #' 
-#' @return A PurpleAir Synoptic \code{pas} object.
+#' @return A PurpleAir Synoptic \emph{pas} object.
 #' 
 #' @seealso \link{pas_loadLatest}
 #' 
@@ -37,18 +40,17 @@
 
 pas_load <- function(
   datestamp = strftime(lubridate::today("America/Los_Angeles"), "%Y%m%d"),
-  baseUrl = "http://smoke.mazamascience.com/data/PurpleAir/pas/2019"
+  baseUrl = "http://smoke.mazamascience.com/data/PurpleAir/pas/2019",
+  retries = 10
 ) {
   
   logger.debug("----- pas_load() -----")
   
   # Validate parameters --------------------------------------------------------
   
-  date <- lubridate::ymd(datestamp)
-  
   if ( datestamp <= "20190404" ) 
     stop("No data is available prior to April 5, 2019.")
-
+  
   # Allow datestamp to be one day past today to handle timezone differences
   tomorrow <- lubridate::today("America/Los_Angeles") + lubridate::ddays(1)
   if ( datestamp > strftime(tomorrow, "%Y%m%d") )
@@ -56,15 +58,29 @@ pas_load <- function(
   
   # Load data from URL ---------------------------------------------------------
   
-  filename <- paste0("pas_", datestamp, ".rda")
-  filepath <- paste0(baseUrl, '/', filename)
-  # Define a 'connection' object so we can close it no matter what happens
-  conn <- url(filepath)
-  result <- try({
-    suppressWarnings(pas <- get(load(conn)))
-  }, silent=TRUE )
-  close(conn)
+  result <- NULL
+  loaded <- FALSE
+  tries <- 0
+  date <- lubridate::ymd(datestamp)
+  
+  # Keep looking back for a valid data file until all tries are used
+  while (!loaded & tries < retries) {
+    datestamp <- strftime(date, "%Y%m%d")
+    filename <- paste0("pas_", datestamp, ".rda")
+    filepath <- paste0(baseUrl, '/', filename)
     
+    # Define a 'connection' object so we can close it no matter what happens
+    conn <- url(filepath)
+    result <- try({
+      suppressWarnings(pas <- get(load(conn)))
+    }, silent=TRUE )
+    close(conn)
+    
+    loaded <- !("try-error" %in% class(result))
+    date <- date - lubridate::days(1)
+    tries <- tries + 1
+  }
+  
   # NOTE:  We used suppressWarnings() above so that we can have a more
   # NOTE:  uniform error response for the large variety of reasons that
   # NOTE:  loading might fail.
@@ -74,7 +90,8 @@ pas_load <- function(
     # TODO:  when logging has not been initialized.
     # # Log the error if logging is enabled. Fail silently otherwise.
     # try({ logger.error("%s", geterrmessage()) }, silent = TRUE)
-    stop(paste0("Data file could not be loaded: ", filepath), call.=FALSE)
+    stop(paste0("Data file could not be loaded after ", retries, " tries"), 
+         call.=FALSE)
   }
   
   return(invisible(pas))
---
+++ tests/testthat/test-pat_filterNear.R
@@ -0,0 +1,5 @@
+context("test-pas_filterNear")
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
