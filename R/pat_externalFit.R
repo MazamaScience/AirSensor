7703bb1a2b57e7079d237f30810c6559f63b0676
Tate Brasel <tate@Shuksan.domain>
2019-07-02 11:01:13 -0700

    pat_externalFit basic functionality

--- R/pat_externalFit.R
+++ R/pat_externalFit.R
@@ -4,7 +4,7 @@
 #' @import dplyr
 #' @import graphics
 #' 
-#' @title Linear model fitting of PurpleAir and federal time series data
+#' @title Linear model fitting of PurpleAir and federal PWFSL time series data
 #' 
 #' @param pat PurpleAir Timeseries \emph{pat} object.
 #' @param replaceOutliers Logical specifying whether or not to replace outliers.
@@ -22,18 +22,20 @@
 #' @param qc_algorithm Named QC algorithm to apply to hourly aggregation stats.
 #' @param min_count Aggregation bins with fewer than `min_count` measurements
 #' will be marked as `NA`.
+#' @param pa_color Color of hourly points
+#' @param pwfsl_color Color of hourly points
 #' 
-#' @description Uses a linear model to fit data from PurpleAir to data from 
-#' a federal monitor.
+#' @description Produces a linear model between data from PurpleAir and data 
+#' from the closest PWFSL monitor.
 #' 
 #' A diagnostic plot is produced if `showPlot = TRUE`.
 #' 
-#' @return A linear model, fitting the `pat` pm25 readings to federal monitor 
-#' readings.
+#' @return A linear model, fitting the `pat` PurpleAir readings to the closest
+#' PWFSL monitor readings.
 #' 
 #' @examples
 #' \dontrun{
-#' pat <- pat_load("SCAH_07")
+#' pat <- pat_load("SCAH_22")
 #' pat_externalFit(pat)
 #' }
 
@@ -44,12 +46,14 @@ pat_externalFit <- function(
   size = 1,
   shape = 15,
   color = "purple",
-  alpha = 0.6,
+  alpha = 0.5,
   xylim = NULL,
   period = "1 hour",
   channel = "ab",
   qc_algorithm = "hourly_AB_01",
-  min_count = 10
+  min_count = 10,
+  pa_color = rgb(0.9, 0.25, 0.2),
+  pwfsl_color = rgb(0.2, 0.25, 0.9)
 ) {
   
   # ----- Validate parameters --------------------------------------------------
@@ -64,22 +68,17 @@ pat_externalFit <- function(
   meta <- pat$meta
   data <- pat$data
   
-  if ( is.null(xylim) ) {
-    dataMin <- min(c(0, data$pm25_A, data$pm25_B), na.rm = TRUE)
-    dataMax <- max(c(data$pm25_A, data$pm25_B), na.rm = TRUE)
-    xylim <- c(dataMin, dataMax)
-  }
-  
   # ----- Assemble data ---------------------------------------------
   
   if ( replaceOutliers )
     pat <- pat_outliers(pat, showPlot = FALSE, replace = TRUE)
   
-  # Get the hourly aggregated data
+  # Get the hourly aggregated PurpleAir data
   paHourly_data <-
     pat %>% 
     pat_createAirSensor(period = "1 hour") %>%
     PWFSLSmoke::monitor_extractData()
+  
   names(paHourly_data) <- c("datetime", "pa_pm25")
   
   tlim <- range(paHourly_data$datetime)
@@ -90,16 +89,28 @@ pat_externalFit <- function(
     PWFSLSmoke::monitor_load(tlim[1], tlim[2], monitorIDs = monitorID) %>%
     PWFSLSmoke::monitor_subset(tlim = tlim) %>%
     PWFSLSmoke::monitor_extractData()
+  
   names(pwfsl_data) <- c("datetime", "pwfsl_pm25")
+  
+  # Combine data from both monitors into one dataframe
+  both_data <- dplyr::full_join(paHourly_data, pwfsl_data, by = "datetime")
   
   # Create a tidy dataframe appropriate for ggplot
   tidy_data <-
-    dplyr::full_join(paHourly_data, pwfsl_data, by = "datetime")
+    both_data %>%
+    tidyr::gather("source", "pm25", -.data$datetime)
+  
+  # Define square xy limit now that we have the data for both monitors
+  if ( is.null(xylim) ) {
+    dataMin <- min(c(0, both_data$pa_pm25, both_data$pwfsl_pm25), na.rm = TRUE)
+    dataMax <- max(c(both_data$pa_pm25, both_data$pwfsl_pm25), na.rm = TRUE)
+    xylim <- c(dataMin, dataMax)
+  }
   
   # ----- Linear model ---------------------------------------------------------
   
-  # Model A as a function of B (data should lie on a line)
-  model <- lm(tidy_data$pwfsl_pm25 ~ tidy_data$pa_pm25, subset = NULL, 
+  # Model PWSFL as a function of PurpleAir (data should lie on a line)
+  model <- lm(both_data$pwfsl_pm25 ~ both_data$pa_pm25, subset = NULL, 
               weights = NULL)
   
   slope <- as.numeric(model$coefficients[2])      # as.numeric() to remove name
@@ -111,37 +122,63 @@ pat_externalFit <- function(
     ggplot2::annotate(
       geom = "text", 
       x = 0.75 * xylim[2],
-      y = c(0.4, 0.3, 0.2) * xylim[2], 
+      y = c(0.25, 0.15, 0.05) * xylim[2], 
       label = c(paste0("Slope = ", round(slope, digits = 2)),
                 paste0("Intercept = ", round(intercept, digits = 1)),
                 paste0("R\U00B2 = ", round(r_squared, digits = 3))) )
   
-  # ----- Plot -----------------------------------------------------------------
+  # ----- Construct Plot -------------------------------------------------------
   
   if ( showPlot ) { 
     
-    pm25_plot <- 
-      tidy_data %>% 
-      ggplot2::ggplot() +
-      ggplot2::geom_point(ggplot2::aes(x = .data$datetime, y = .data$pwfsl_pm25),
-                          size = size,
+    # LH Linear regression plot
+    lm_plot <- 
+      both_data %>% 
+      ggplot2::ggplot(ggplot2::aes(x = .data$pa_pm25, y = .data$pwfsl_pm25)) + 
+      ggplot2::geom_point(size = size, 
                           shape = shape,
-                          color = rgb(0.9, 0.25, 0.2), # pat_multiplot default
-                          alpha = alpha) +
+                          color = color,
+                          alpha = alpha) + 
+      ggplot2::geom_smooth(method = "lm", color = "gray80", alpha = 1.0) + 
+      ggplot2::labs(title = "PWSFL vs. PurpleAir", 
+                    x = "PurpleAir PM 2.5 (\u03bcg / m\u00b3)", 
+                    y = "PWFSL PM 2.5 (\u03bcg / m\u00b3)") + 
+      ggplot2::theme_bw() + 
+      ggplot2::xlim(xylim) +
+      ggplot2::ylim(xylim) +
+      ggplot2::coord_fixed() +    # square aspect ratio
+      equationLabel
+    
+    # Labels
+    timezone <- pat$meta$timezone[1]
+    year <- strftime(pat$data$datetime[1], "%Y", tz=timezone)
+    
+    pm25_plot <- 
+      both_data %>% 
+      ggplot2::ggplot() +
       ggplot2::geom_point(ggplot2::aes(x = .data$datetime, y = .data$pa_pm25),
                           size = size,
                           shape = shape,
-                          color = rgb(0.2, 0.25, 0.9), # pat_multiplot default
+                          color = pa_color, # pat_multiplot default
+                          alpha = alpha) +
+      ggplot2::geom_point(ggplot2::aes(x = .data$datetime, 
+                                       y = .data$pwfsl_pm25),
+                          size = size,
+                          shape = shape,
+                          color = pwfsl_color, # pat_multiplot default
                           alpha = alpha) +
       ggplot2::ylim(xylim) +
-      ggplot2::ggtitle(expression("Channel A/B PM"[2.5])) #+ 
-      #ggplot2::xlab(year) + ggplot2::ylab("\u03bcg / m\u00b3") 
-
-    print(pm25_plot)
+      ggplot2::ggtitle(expression("Monitor PM"[2.5])) + 
+      ggplot2::xlab(year) + ggplot2::ylab("\u03bcg / m\u00b3") 
+    
+    plot <- multi_ggplot(lm_plot, pm25_plot, 
+                         cols = 1, plotList = NULL)
+    
+    print(plot)
     
   }
   
-  return(model)
+  return(invisible(model))
   
 }
 
