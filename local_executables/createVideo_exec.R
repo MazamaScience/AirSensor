dcc6e44f82c5fa51f8224a68fb801d4f3dcb5074
Tate Brasel <tate@Shuksan.domain>
2019-07-23 13:40:57 -0700

    options for community name or id

--- local_executables/createVideo_exec.R
+++ local_executables/createVideo_exec.R
@@ -7,7 +7,8 @@
 #
 # Test this script from the command line with:
 #
-# ./createVideo_exec.R --community="Sycamore Canyon" -s 20190704 -r 4 -d ~/Desktop
+# ./createVideo_exec.R --communityName="Sycamore Canyon" -s 20190704 -r 4 -d ~/Desktop/ -v TRUE
+# ./createVideo_exec.R -c SCSB -d ~/Desktop/
 
 VERSION = "0.1.0"
 
@@ -22,6 +23,12 @@ suppressPackageStartupMessages({
 
 if ( interactive() ) {
   
+  # RStudio session
+  opt <- list(outputDir = getwd(),
+              logDir = getwd(),
+              spatialDataDir = "~/Data/Spatial",
+              version = FALSE)  
+  
 } else {
   
   # Set up OptionParser
@@ -29,24 +36,44 @@ if ( interactive() ) {
   
   option_list <- list(
     make_option(
-      c("-c","--community"), 
+      c("-c","--communityID"), 
       default="", 
-      help="Name of South Coast community [default=\"%default\"]"
+      help="ID of the South Coast community [default=\"%default\"]"
     ),
     make_option(
-      c("-s","--startDate"), 
+      c("-C","--communityName"), 
+      default="", 
+      help="Name of the South Coast community [default=\"%default\"]"
+    ),
+    make_option(
+      c("-S","--startDate"), 
       default=0, 
-      help="Start date for the 3-day period [default=\"%default\"]"
-    ),
-    make_option(
-      c("-d", "--directory"),
-      default="./",
-      help="Path of directory to save video to [default=\"%default\"]"
+      help="Start date for the 3-day (72 hr) period [default=\"%default\"]"
     ),
     make_option(
       c("-r", "--frameRate"),
       default=6,
       help="Frames per second [default=\"%default\"]"
+    ),
+    make_option(
+      c("-v","--verbose"), 
+      default=FALSE, 
+      help="Print out generated frame files [default=\"%default\"]"
+    ),
+    make_option(
+      c("-o","--outputDir"), 
+      default=getwd(), 
+      help="Output directory for generated video file [default=\"%default\"]"
+    ),
+    make_option(
+      c("-l","--logDir"), 
+      default=getwd(), 
+      help="Output directory for generated .log file [default=\"%default\"]"
+    ),
+    make_option(
+      c("-s","--spatialDataDir"), 
+      default="/home/mazama/data/Spatial", 
+      help="Directory containing spatial datasets used by MazamaSpatialUtils [default=\"%default\"]"
     ),
     make_option(
       c("-V","--version"), 
@@ -66,91 +93,21 @@ if (opt$version) {
   quit()
 }
 
-if (opt$community == "") {
-  cat(paste0("Community name must be given\n"))
-  quit()
-}
-
 # ----- Validate parameters ----------------------------------------------------
-
-# SCAP --- Alhambra/Monterey Park
-# SCBB --- Big Bear Lake
-# SCEM --- El Monte
-# SCIV --- Imperial Valley
-# SCNP --- Nipomo
-# SCPR --- Paso Robles
-# SCSJ --- San Jacinto
-# SCSB --- Seal Beach
-# SCAH --- SCAH
-# SCAN --- SCAN
-# SCUV --- SCUV
-# SCSG --- South Gate
-# SCHS --- Sycamore Canyon
-# SCTV --- Temescal Valley
 
 if (opt$frameRate < 0 || opt$frameRate != floor(opt$frameRate)) {
   stop("frameRate must be a positive integer")
 }
 
-if (opt$community == "Alhambra/Monterey Park") {
-  lon <- -118.132324 
-  lat <- 34.072205
-  z <- 13
-} else if (opt$community == "Big Bear Lake") {
-  lon <- -116.898568 
-  lat <- 34.255736
-  z <- 13
-} else if (opt$community == "El Monte") {
-  lon <- -118.034595
-  lat <- 34.069292
-  z <- 12
-} else if (opt$community == "Imperial Valley") {
-  lon <- -115.551228
-  lat <- 32.980878
-  z <- 14
-} else if (opt$community == "Nipomo") {
-  lon <- -120.555047
-  lat <- 35.061590
-  z <- 12
-} else if (opt$community == "Paso Robles") {
-  lon <- -120.668946
-  lat <- 35.513530
-  z <- 10
-} else if (opt$community == "San Jacinto") {
-  lon <- -116.958228
-  lat <- 33.765083
-  z <- 14
-} else if (opt$community == "SCAH") {
-  lon <- -122.139473
-  lat <- 37.662620
-  z <- 10
-} else if (opt$community == "SCAN") {
-  lon <- -122.307492
-  lat <- 37.964949
-  z <- 12
-} else if (opt$community == "SCUV") {
-  lon <- -118.427781
-  lat <- 34.023917
-  z <- 15
-} else if (opt$community == "Seal Beach") {
-  lon <- -118.083
-  lat <- 33.767
-  z <- 15
-} else if (opt$community == "South Gate") {
-  lon <- -118.178104
-  lat <- 33.934260
-  z <- 13
-} else if (opt$community == "Sycamore Canyon") {
-  lon <- -117.307598
-  lat <- 33.947524
-  z <- 15
-} else if (opt$community == "Temescal Valley") {
-  lon <- -117.481278
-  lat <- 33.753517
-  z <- 12
-} else {
-  stop(paste0("There is no SC community '", opt$community, "'"))
+if (opt$communityID == "" && opt$communityName == "") {
+  stop("Must define either a community name or ID")
 }
+
+if ( !dir.exists(opt$outputDir) ) 
+  stop(paste0("outputDir not found:  ",opt$outputDir))
+
+if ( !dir.exists(opt$logDir) ) 
+  stop(paste0("logDir not found:  ",opt$logDir))
 
 # ----- Set up logging ---------------------------------------------------------
 
@@ -158,10 +115,65 @@ if (opt$community == "Alhambra/Monterey Park") {
 
 result <- try({
   
-  # Load data
-  sensor <- sensor_load()
-  timeZone <- dplyr::filter(sensor$meta, communityRegion == opt$community)[1, "timezone"]
+  # SCAP --- Alhambra/Monterey Park
+  # SCBB --- Big Bear Lake
+  # SCEM --- El Monte
+  # SCIV --- Imperial Valley
+  # SCNP --- Nipomo
+  # SCPR --- Paso Robles
+  # SCSJ --- San Jacinto
+  # SCSB --- Seal Beach
+  # SCAH --- SCAH
+  # SCAN --- SCAN
+  # SCUV --- SCUV
+  # SCSG --- South Gate
+  # SCHS --- Sycamore Canyon
+  # SCTV --- Temescal Valley
   
+  communityGeoMapInfo <- list(
+    SCAP = list(lon = -118.132324, lat = 34.072205, zoom = 13),
+    SCBB = list(lon = -116.898568, lat = 34.255736, zoom = 13),
+    SCEM = list(lon = -118.034595, lat = 34.069292, zoom = 12),
+    SCIV = list(lon = -115.551228, lat = 32.980878, zoom = 14),
+    SCNP = list(lon = -120.555047, lat = 35.061590, zoom = 12),
+    SCPR = list(lon = -120.668946, lat = 35.513530, zoom = 10),
+    SCSJ = list(lon = -116.958228, lat = 33.765083, zoom = 14),
+    SCSB = list(lon = -118.083084, lat = 33.767033, zoom = 15),
+    SCAH = list(lon = -122.139473, lat = 37.662620, zoom = 10),
+    SCAN = list(lon = -122.307492, lat = 37.964949, zoom = 12),
+    SCUV = list(lon = -118.427781, lat = 34.023917, zoom = 15),
+    SCSG = list(lon = -118.178104, lat = 33.934260, zoom = 13),
+    SCHS = list(lon = -117.307598, lat = 33.947524, zoom = 15),
+    SCTV = list(lon = -117.481278, lat = 33.753517, zoom = 12)
+  )
+  
+  # Load all sensor data
+  sensor <- sensor_load()
+  
+  # Retreive both the community name and ID. Prioritize name over ID if they are
+  # different.
+  if (opt$communityName != "") {
+    communityMeta <- dplyr::filter(sensor$meta, communityRegion == opt$communityName)
+    if (nrow(communityMeta) == 0) {
+      stop(paste0("Community with name '", opt$communityName, "' has no monitors"))
+    }
+    opt$communityID <- toupper(sub("\\_.*", "", communityMeta[1, "monitorID"]))
+  } else if (opt$communityID != "") {
+    opt$communityID <- toupper(opt$communityID)
+    communityMeta <- dplyr::filter(sensor$meta, stringr::str_detect(toupper(monitorID), paste0("^", opt$communityID, "_")))
+    if (nrow(communityMeta) == 0) {
+      stop(paste0("Community with ID '", opt$communityID, "' has no monitors"))
+    }
+    opt$communityName <- communityMeta[1, "communityRegion"]
+  } else {
+    stop("Must provide a South Coast community name or ID")
+  }
+  
+  timeZone <- dplyr::filter(sensor$meta, 
+                            communityRegion == opt$communityName)[1, "timezone"]
+  
+  # Look forward 71 hours from startdate entry, or back 71 hours from the most 
+  # recent data entry if no startdate is given
   if (opt$startDate > 0) {
     start <- lubridate::parse_date_time(opt$startDate, orders = "ymd", tz = timeZone)
     end   <- start + lubridate::hours(71)
@@ -170,49 +182,62 @@ result <- try({
     start <- end - lubridate::hours(71)
   }
   
-  movieData <- sensor_filter(sensor, datetime >= start, datetime <= end)
+  mapInfo <- communityGeoMapInfo[[opt$communityID]]
   
   # Time axis data
   tickSkip <- 6
+  movieData <- sensor_filter(sensor, datetime >= start, datetime <= end)
   tAxis <- movieData$data$datetime
   tAxis[(lubridate::hour(tAxis) - 1) %% tickSkip == 0 & 
            lubridate::minute(tAxis) == 0]
   tTicks <- tAxis[(lubridate::hour(tAxis) - 1) %% tickSkip == 0 & 
                      lubridate::minute(tAxis) == 0]
   tLabels <- strftime(tTicks, "%l %P")
-  tInfo <- PWFSLSmoke::timeInfo(tAxis, longitude = lon, latitude = lat)
+  tInfo <- PWFSLSmoke::timeInfo(tAxis, longitude = mapInfo$lon, latitude = mapInfo$lat)
   
-  # Load static map image of community
-  staticMap <- PWFSLSmoke::staticmap_getStamenmapBrick(centerLon = lon,
-                                                       centerLat = lat,
-                                                       zoom = z,
-                                                       width = 770,
-                                                       height = 495)
-  
-  # Get the community's South Coast ID for labeling the frames and video
-  communityID <- sub("\\_.*", "", dplyr::filter(sensor$meta, communityRegion == opt$community)[1, "monitorID"])
-  communityID <- toupper(communityID)
+  # Load a static map image of the community
+  staticMap   <- PWFSLSmoke::staticmap_getStamenmapBrick(centerLon = mapInfo$lon,
+                                                         centerLat = mapInfo$lat,
+                                                         zoom = mapInfo$zoom,
+                                                         width = 770,
+                                                         height = 495)
   
   # Generate individual frames
   for (i in 1:length(tAxis)) {
     ft <- tAxis[i]
     number <- stringr::str_pad(i, 3, 'left', '0')
-    fileName <- paste0(communityID, number, ".png")
+    fileName <- paste0(opt$communityID, number, ".png")
     filePath <- file.path(tempdir(), fileName)
     png(filePath, width = 1280, height = 720, units = "px")
     sensor_videoFrame(sensor,
-                      communityRegion = opt$community,
+                      communityRegion = opt$communityName,
                       frameTime = ft,
                       timeInfo = tInfo,
                       timeAxis = tAxis,
                       timeTicks = tTicks,
                       timeLabels = tLabels,
                       map = staticMap)
-    print(strftime(ft, "%b %d %H:%M"))
+    if (opt$verbose) {
+      print(strftime(ft, "%b %d %H:%M"))
+    }
     dev.off()
   }
   
+  # Define system calls to ffmpeg to create video from frames
+  cmd_cd <- paste0("cd ", tempdir())
+  cmd_ffmpeg <- paste0("ffmpeg -r ", opt$frameRate, " -f image2 -s 1280x720 -i ", 
+                       opt$communityID, "%03d.png -vcodec libx264 -crf 25 ", 
+                       opt$outputDir, "/", opt$communityID, ".mp4")
+  cmd_rm <- paste0("rm *.png")
+  
+  # Make system calls
+  system(paste0(cmd_cd, " && ", cmd_ffmpeg, " && ", cmd_rm))
+  
 }, silent=TRUE)
+
+if (opt$verbose) {
+  print(result)
+}
 
 # Handle errors
 if ( "try-error" %in% class(result) ) {
@@ -223,13 +248,3 @@ if ( "try-error" %in% class(result) ) {
   #if ( !file.exists(errorLog) ) dummy <- file.create(errorLog)
   #logger.info("Completed successfully!")
 }
-
-# Define system calls to ffmpeg to create video from frames
-cmd_cd <- paste0("cd ", tempdir())
-cmd_ffmpeg <- paste0("ffmpeg -r ", opt$frameRate, " -f image2 -s 1280x720 -i ", 
-                     communityID, "%03d.png -vcodec libx264 -crf 25 ", 
-                     opt$directory, "/", communityID, ".mp4")
-cmd_rm <- paste0("rm *.png")
-
-# Make system calls
-system(paste0(cmd_cd, " && ", cmd_ffmpeg, " && ", cmd_rm))
