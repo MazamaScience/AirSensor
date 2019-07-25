f7e348080b6f506fde75245404c356154c0a67e6
Tate Brasel <tate@Shuksan.domain>
2019-07-25 13:54:42 -0700

    auto wind loading for rose plots

--- R/sensor_pollutionRose.R
+++ R/sensor_pollutionRose.R
@@ -48,11 +48,11 @@
 #'
 #' @examples
 #' \donttest{
-#' # Use example sensor, whose date range is 2018-08-01 to 2018-10-01
-#' sensor <- example_sensor
+#' sensor <- sensor_load(startdate = 20190601, enddate = 20190630)
+#' sensor <- sensor_filterMeta(sensor, monitorID == "SCSB_02")
 #' 
-#' # Load wind data from NOAA. 'parallel' cannot be true in examples.
-#' windData <- worldmet::importNOAA(code = "722975-53141", year = 2018, 
+#' # Load wind data from NOAA
+#' windData <- worldmet::importNOAA(code = "722975-53141", year = 2019, 
 #'                                  parallel = FALSE)
 #' windData <- dplyr::select(windData, c("date", "wd", "ws"))
 #' 
@@ -63,7 +63,7 @@
 sensor_pollutionRose <- 
   function(
     sensor, 
-    windData,
+    windData = NULL,
     statistic = "prop.count",
     key = TRUE, 
     keyPosition = "right",
@@ -84,18 +84,28 @@ sensor_pollutionRose <-
     if ( PWFSLSmoke::monitor_isEmpty(sensor) ) 
       stop("Required parameter 'sensor' has no data.")
     
-    if ( !all((c("wd", "ws") %in% names(windData))) ) 
-      stop("Parameter 'windData' does not contain necessary columns")
+    if ( nrow(sensor$meta) == 0 )
+      stop("Parameter 'sensor' contains no SC sensors")
     
+    if ( nrow(sensor$meta) > 1 )
+      stop("Parameter 'sensor' contains more than one SC sensor")
     
-    # Find closest NOAA site wind data if none is provided
+    # Find wind data readings from the closest NOAA site if none are provided
     if ( is.null(windData) ) {
+      # Using only the first entry's datetime for the year will be problematic 
+      # if the timeframe spans more than one year...
+      year <- lubridate::year(sensor$data$datetime[1])
+      lon <- sensor$meta$longitude[1]
+      lat <- sensor$meta$latitude[1]
       
+      closestSite <- worldmet::getMeta(lon = lon, lat = lat, n = 1, 
+                                       plot = FALSE)[1,]
+      siteCode <- paste0(closestSite$USAF, "-", closestSite$WBAN)
+      
+      siteData <- worldmet::importNOAA(code = siteCode, year = year, 
+                                       parallel = FALSE)
+      windData <- dplyr::select(siteData, c("date", "wd", "ws"))
     }
-    
-    
-    
-    
 
     # Data must be the same length
     pollutantData <- 
@@ -105,9 +115,9 @@ sensor_pollutionRose <-
       )
     
     # Trim wind data to the sensor's time range
-    windData <- filter(windData, 
-                       date >= min(sensor$data$datetime),
-                       date <= max(sensor$data$datetime))
+    windData <- dplyr::filter(windData, 
+                              date >= min(sensor$data$datetime),
+                              date <= max(sensor$data$datetime))
     
     # Combine df's 
     data <- 
