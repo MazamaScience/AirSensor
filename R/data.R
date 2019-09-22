92478967f82f9444889819538be1e60c93cdf6d1
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-09-22 11:26:06 -0700

    version bump to 0.5.0

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' @title Example Purple Air Synoptic dataset
+#' @format A tibble with 7113 rows and 35 columns of data.
+#' @description The \code{example_pas} dataset provides a quickly loadable
+#' version of a \emph{pa_synoptic} object for practicing and code examples
+#' This dataset was generatedon 2019-07-06 by running:
+#' 
+#' \preformatted{
+#'   initializeMazamaSpatialUtils()
+#'   example_pas <- pas_createNew()
+#' }
+#' 
+#' @seealso example_pas_raw
+#' @source https://www.purpleair.com/json
+"example_pas"
+
