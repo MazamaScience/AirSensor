3ef233ff0a7d4306852c66bd53bec1c7a3de5f62
Kayleigh Wilson <kayleigh@Unknown.domain>
2019-09-25 13:02:42 -0700

    changes to the version of R created conflicts when using &&, adding all() seemed to fix the issue

--- tests/testthat/test-pas_filterArea.R
+++ tests/testthat/test-pas_filterArea.R
@@ -12,17 +12,19 @@ test_that("pa_synoptic class returned", {
 })
 
 test_that("bound order doesn't matter", { 
-  areaOrder1 <- pas_filterArea(example_pas, w = -118.10, e = -118.07, 
+  areaOrder1 <- pas_filterArea(example_pas, 
+                               w = -118.10, e = -118.07, 
                                s = 33.75, n = 33.78)
   lonRange1 <- range(areaOrder1$longitude)
   latRange1 <-range(areaOrder1$latitude)
   
-  areaOrder2 <- pas_filterArea(example_pas, n = 33.78, e = -118.07, 
+  areaOrder2 <- pas_filterArea(example_pas, 
+                               n = 33.78, e = -118.07, 
                                s = 33.75, w = -118.10)
   lonRange2 <- range(areaOrder2$longitude)
   latRange2 <-range(areaOrder2$latitude)
   
-  expect_true(lonRange1 == lonRange2 && latRange1 == latRange2)
+  expect_true(all(lonRange1 == lonRange2) && all(latRange1 == latRange2))
 })
 
 test_that("results_are_consistent", {
