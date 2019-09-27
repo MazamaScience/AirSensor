562e98227ff81c16c47d106f2bc24dca827ea4cd
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-09-27 15:02:30 -0700

    removed test-initializeMazamaSpatialUtils.R

--- tests/testthat/test-initializeMazamaSpatialUtils.R
+++ tests/testthat/test-initializeMazamaSpatialUtils.R
@@ -1,12 +1,12 @@
-context("test-initializeMazamaSpatialUtils")
-
-
-test_that("A spatial data directory exists", {
-  expect_true(dir.exists("~/Data/Spatial")) 
-})
-
-test_that("Spatial Data is loaded", {
-  initializeMazamaSpatialUtils()
-  expect_true(exists("NaturalEarthAdm1"))
-})
+# context("test-initializeMazamaSpatialUtils")
+# 
+# 
+# test_that("A spatial data directory exists", {
+#   expect_true(dir.exists("~/Data/Spatial")) 
+# })
+# 
+# test_that("Spatial Data is loaded", {
+#   initializeMazamaSpatialUtils()
+#   expect_true(exists("NaturalEarthAdm1"))
+# })
 
