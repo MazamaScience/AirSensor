d0952997a54987fb30fc1232faaf2c8a0c9da996
Kayleigh Wilson <kayleigh@Unknown.domain>
2019-10-01 11:54:41 -0700

    plot_agg_stat function WIP

--- local_kayleigh/plot_agg_stat.R
+++ local_kayleigh/plot_agg_stat.R
@@ -3,6 +3,9 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(forcats)
+library(stringr)
+
+setArchiveBaseUrl("http://smoke.mazamascience.com/data/PurpleAir")
 
 # Get pas for Louisiana
 pas <- pas_load()
@@ -11,33 +14,42 @@ pas_la <- pas %>% pas_filter(stateCode == "LA")
 # Get the pat of interest
 pat_la <- pat_createNew(pas_la, label = "Island Park")
 
-# Check out the data
-pat_multiplot(pat_la)
-pat_dygraph(pat_la)
-pat_scatterplot(pat_la)
+# Check out the data ---
+# pat_multiplot(pat_la)
+# pat_dygraph(pat_la)
+# pat_scatterplot(pat_la)
 
 # Create hourly aggregation statistics
 agg_out <- pat_aggregateOutlierCounts(pat_la)
-agg <- pat_aggregate(pat_la)
+# agg <- pat_aggregate(pat_la)
 
 # Sorting names for plot: -> currently runs and works, just doesnt't transfer to
 # ggplot properly
-a<-sort(names(agg))
-a[!a %in% c("pm25_df", "pm25_p", "pm25_t")]
-append(a, c("pm25_df", "pm25_p", "pm25_t"))
+a <- sort(names(agg_out))
+a <- a[!a %in% c("pm25_df", "pm25_p", "pm25_t")]
+a <- append(a, c("pm25_df", "pm25_p", "pm25_t"))
+
+# a <- sort(names(agg_out)) %>%
+#   str_remove_all(.,  c("pm25_df|pm25_p|pm25_t")) %>%
+#   append(c("pm25_df", "pm25_p", "pm25_t"))
+
 
 # Convert from "wide" to "long" (aka "tidy")
 data_long <- agg_out %>%
-  gather(param, value, -datetime)  %>%
-  arrange(match(param, a)) # sort custom order, works but doesn't tranfer to ggplot
+  gather(param, value, -datetime)  
 
-# Create plot
-# gg <- ggplot(data_long, aes(x = datetime, y = value)) +
-#   geom_line() +
-#   labs(title="Aggregation Statistics") +
-#   facet_wrap(~param)
+param <- factor(data_long$param, levels = a)
+
 ggplot(data_long, aes(x = datetime, y = value)) +
   geom_line() +
   labs(title="Aggregation Statistics") +
   #facet_wrap(~param, scales = "free_y")
-  facet_wrap(~param, nrow = 5, scales = "free_y" )
+  facet_wrap(param, nrow = 5, scales = "free_y" )
+
+
+
+
+
+
+
+
