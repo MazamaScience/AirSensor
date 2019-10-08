ef19a70c9647a8d6fc9e59427bf98467c4e25fb7
Kayleigh Wilson <kayleigh@Adams.domain>
2019-10-08 10:19:01 -0700

    Finished SoH_pctReporting()

--- local_kayleigh/state_of_health_testing.R
+++ local_kayleigh/state_of_health_testing.R
@@ -17,30 +17,40 @@ pat2 <- pat_createNew(pas_state, label = "Whitworth University Soccer/Softball C
 pat3 <- pat_createNew(pas_state, label = "TimberlakesCC")
 pat4 <- pat_createNew(pas_state, label = "Lynden")
 pat5 <- pat_createNew(pas_state, label = "North Richland")
+pat6 <- pat_load("SCSB_20", 20190101, 20191010)
+
 
 # put the different sensors into a list:
 pat_list <- list(pat1, pat2, pat3, pat4, pat5)
 names(pat_list) <- c("pat1", "pat2", "pat3", "pat4", "pat5")
 
 # pre-define a list of dataframes to store the agg-stats
-aggregationStats <- list(data.frame())
+#aggregationStats <- list(data.frame())
+aggregationStatsList <- list()
 
 # agg_stats for one station for testing:
-agg_stat_test <- pat_aggregateOutlierCounts(pat1)
+agg_stat_test <- pat_aggregateOutlierCounts(pat6)
 
 # calculate agg stats for each station
 for (i in seq_along(pat_list)) {
-  aggregationStats[[i]] <- pat_aggregateOutlierCounts(pat_list[[i]])
+  aggregationStatsList[[i]] <- pat_aggregateOutlierCounts(pat_list[[i]])
 }
 
-PurpleAirQC_aggregationPlot(aggregationStats[[1]], ylim = "free_y")
+PurpleAirQC_aggregationPlot(aggregationStats[[1]], autoRange = TRUE)
 
 
 # Begin SOH_pctReporting()
 
-samplingFreq <- 30
-total_pm25ACount <- summarise(agg_stat_test, sum(agg_stat_test$pm25_A_count))
-pctReporting <- total_pm25ACount/samplingFreq
+samplingFreq <- 30 
+samplesperDay <- samplingFreq*24
+tbl <- 
+  agg_stat_test %>%
+  mutate(day = as.Date(datetime, format="%Y-%m-%d")) %>%
+  group_by(day) %>%
+  summarise(daily_sum = sum(pm25_A_count)) %>%
+  mutate(pct_Reporting = daily_sum/samplesperDay*100)
+
+#pct_Reporting <- total_pm25ACount/samplingFreq
 
 
 
