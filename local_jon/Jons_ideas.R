dc7b77dd0581c7f827157ffe7eb45c6a6f791a51
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2019-11-05 17:29:00 -0800

    removed satellite related examples

--- local_jon/Jons_ideas.R
+++
@@ -1,9 +0,0 @@
-
-# Thinking about the example in goesaodc_createHourlyRasterStack.R, it sure
-# would be nice to have an outline of Oregon.
-#
-# Perhaps the answer is here, using the 'panel' argument to 
-# rasterVis::levelplot():
-#
-#   http://r-sig-geo.2731867.n2.nabble.com/how-to-use-levelplot-with-a-geographic-projection-td7584460.html
-
