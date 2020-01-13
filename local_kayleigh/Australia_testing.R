82bbddf5adb4f596fe85bc4e5df5b96c5985709c
Kayleigh Wilson <kayleigh@Adams.domain>
2020-01-13 14:00:49 -0800

    #WIP Australia. In process of converting to RMD.

--- local_kayleigh/Australia_testing.R
+++ local_kayleigh/Australia_testing.R
@@ -27,41 +27,70 @@ label_au <- c("PIE Wagga", "MORUYA HEADS", "Hamilton ", "Gadd Street Air Quality
 
 
 pat_test <- pat_createNew(pas = pas_au, label = "PIE Wagga")
-deviceDeploymentID <- rep(0, length(label_au))
 
-for (i in seq(label_au)){
-  deviceDeploymentID[i] <-
-    pas_au %>%
-    pas_filter(.data$DEVICE_LOCATIONTYPE == "outside") %>%
-    pas_filter(is.na(.data$parentID)) %>%
-    pas_filter(.data$countryCode == "AU") %>%
-    pas_filter(.data$label == label_au[i]) %>%
-    dplyr::pull(deviceDeploymentID)
-  
-}
+label <- c("Gadd Street Air Quality")
 
-ql_ids <- pas_getDeviceDeploymentIDs(pas_au, countryCodes = c("AU"))
+deviceDeploymentID <- pas_getDeviceDeploymentIDs(pas = pas, 
+                                                 countryCodes = c("AU"), 
+                                                 pattern = label)
 
-pat_gadd <- pat_createNew(pas = pas_au, id = deviceDeploymentID[4], startdate = 20191201, enddate = 20200108)
+testdeviceID <- pas_getDeviceDeploymentIDs(pas = pas, 
+                                           countryCodes = c("AU"), 
+                                           pattern = "Werribbee Street North Park")
 
-pat_multiplot(pat_para)
+pat_gadd <- pat_createNew(pas = pas, id = deviceDeploymentID, 
+                          startdate = 20191115, enddate = 20200108)
 
-pat <- pat_createNew("01309a490e3cf5b4_36471", label = NULL, pas = pas_au, startdate = 20191101, enddate = 20191231)
+pat_test <- pat_createNew(pas = pas, id = testdeviceID, 
+                          startdate = 20191115, enddate = 20200108)
+
+pat_multiplot(pat_test)
+
 
 lon <- pat_gadd$meta$longitude
 lat <- pat_gadd$meta$latitude
 
 pas_melbourne <- 
   pas_au %>%
-  pas_filterNear(longitude = lon, latitude = lat, radius = "60 km") %>%
-  pas_filter(.data$DEVICE_LOCATIONTYPE == "outside")
+  pas_filterNear(longitude = lon, latitude = lat, radius = "60 km") 
 
 pas_leaflet(pas_melbourne)
-View(pas_melbourne)
 
-pat_externalFit(pat_gadd)
+labels <- pas_getLabels(pas_melbourne, countryCodes = c("AU"))
+deviceDeploymentIDs <- pas_getDeviceDeploymentIDs(pas_melbourne, countryCodes = c("AU"))
+IDsubset <- deviceDeploymentIDs[1:3]
+
+patList <- list()
+for ( id in IDsubset ) { 
+  patList[[id]] <- pat_createNew(id, pas = pas_melbourne, startdate = 20191225, enddate = 20200108) }
+
+pat_beacon <- patList[[1]]
+pat_werribee <- patList[[2]]
+pat_donvale <- patList[[3]]
+
+pat_multiplot(pat_donvale)
+
+
+ pat_externalFit(pat_gadd)
 pat_internalFit(pat_gadd)
 pat_dailySoHIndexPlot(pat_gadd)
+
+# deviceDeploymentID <- list()
+# for (i in seq(label_au)){
+#   deviceDeploymentID[i] <- pas_getDeviceDeploymentIDs(pas = pas_au, countryCodes = c("AU"), pattern = label_au[i])
+# }
+
+# deviceDeploymentID <- rep(0, length(label_au))
+# 
+# for (i in seq(label_au)){
+#   deviceDeploymentID[i] <-
+#     pas_au %>%
+#     pas_filter(.data$DEVICE_LOCATIONTYPE == "outside") %>%
+#     pas_filter(is.na(.data$parentID)) %>%
+#     pas_filter(.data$countryCode == "AU") %>%
+#     pas_filter(.data$label == label_au[i]) %>%
+#     dplyr::pull(deviceDeploymentID)
+# }
 
 #---- Generic testing -----
 setArchiveBaseUrl("http://smoke.mazamascience.com/data/PurpleAir")
@@ -88,6 +117,6 @@ pas_test_au <- pas_createNew(
   lookbackDays = 1
 )
 
-pat_test <- pat_createNew(pas = pas_test_au, label = "PIE Wagga")
+pat_test <- pat_createNew(pas = pas_au, label = "PIE Wagga")
 
 
