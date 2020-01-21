ca0d67c4e8ebb0830000528f049c3cc1b69804f7
Kayleigh Wilson <kayleigh@Adams.domain>
2020-01-21 12:00:39 -0800

    finished updating vignettes to use new device deployment ID's

--- local_kayleigh/vignette_testing.R
+++ local_kayleigh/vignette_testing.R
@@ -6,7 +6,7 @@ library(ggplot2)
 pas <- AirSensor::example_pas
 pat <- AirSensor::example_pat
 
-id <- 
+id_seattle <- 
   pas %>%
   pas_filter(label == "Seattle") %>%
   dplyr::pull(deviceDeploymentID)
@@ -16,6 +16,77 @@ id_home <-
   pas_filter(label == "home") %>%
   dplyr::pull(deviceDeploymentID)
 
-pat <- pat_createNew(id = id, pas = pas, startdate = 20180701, enddate = 20180901)
+id <- pas_getDeviceDeploymentIDs(pas = pas, pattern = "Seattle")
+id <- pas_getDeviceDeploymentIDs(pas, "^Seattle$")
+id_home <- pas_getDeviceDeploymentIDs(pas, "^Home$")
+
+pat_test <- pat_createNew(id = id, pas = pas, startdate = 20180701, enddate = 20180901)
   
 pat <- pat_createNew(label = "Seattle", pas = pas, startdate = 20180701, enddate = 20180901)
+
+
+## purpleair_failure_modes
+
+setArchiveBaseUrl("https://airfire-data-exports.s3-us-west-2.amazonaws.com/PurpleAir/v1")
+#setArchiveBaseUrl("http://smoke.mazamascience.com/data/PurpleAir")
+pas <- pas_load(archival = TRUE)
+pas <- example_pas
+noisy_2_id <- pas_getDeviceDeploymentIDs(pas, pattern = "SCAP_14")
+pat_a_noisy_2 <- pat_createNew(pas, 
+                               id = noisy_2_id,
+                               label = "SCAP_14", 
+                               startdate = 20190701, 
+                               enddate = 20190708,
+                               timezone = "America/Los_Angeles")
+humidity_id <- pas_getDeviceDeploymentIDs(pas, 
+                                          pattern = "BikeSGV - West Pasadena")
+
+id <- pas_getDeviceDeploymentIDs(pas, pattern = "SCEM_05")
+
+
+zero_id <- pas_getDeviceDeploymentIDs(pas, pattern = "SCAP_46")
+
+# Australia 
+
+archiveBaseDir <- path.expand("~/Data/Australia_on_fire")
+if ( !dir.exists(archiveBaseDir) ) {
+  dir.create(archiveBaseDir)
+}
+
+setArchiveBaseDir(archiveBaseDir)
+
+filePath_pas <- file.path(archiveBaseDir, "pas_au.rda")
+
+if ( !file.exists(filePath_pas) ) {
+  initializeMazamaSpatialUtils()
+  pas_au <- pas_createNew(countryCodes = "AU", includePWFSL = TRUE)
+  save(pas_au, file = filePath_pas)
+}
+
+pas <- get(load(filePath_pas))
+
+
+jannali_id <- pas_getDeviceDeploymentIDs(pas, pattern = "Jannali")
+
+
+unhealthy_labels <- c("Chisholm ACT Australia", 
+                      "MORUYA HEADS",
+                      "Windang, Ocean Street")
+
+patList <- list()
+for ( label in unhealthy_labels ) {
+  id <- pas_getDeviceDeploymentIDs(pas, pattern = label)
+  patList[[label]] <- pat_createNew(id = id, 
+                                    label = label,
+                                    pas = pas_au, 
+                                    startdate = 20191210, 
+                                    enddate = 20200110) 
+}
+
+
+
+
+
+
+
+
