6eeed0708bd7cff2f1c3b4b9cf406dd8e80093b2
Hans <hansmrtn@gmail.com>
2020-04-14 15:51:15 -0700

    Split up functions issue #206

---
+++ R/pas_addAirDistrict.R
@@ -0,0 +1,88 @@
+#' @importFrom MazamaCoreUtils logger.isInitialized logger.trace logger.warn logger.error logger.fatal
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @title Add an air district to PurpleAir Synoptic Data
+#'
+#' @description Adds an air district (if any) to a pa_synoptic object via the 
+#' \code{MazamaSpatialUtils} Package using PurpleAir location coordinates to 
+#' determine the air basin the sensor is in. 
+#' 
+#' @note As of 2020-04-14, only California air basins is supported. 
+#'
+#' @param pas PurpleAir Synoptic \emph{pas} object. 
+#'
+#' @return A pa_synoptic dataframe
+#' 
+#' @seealso \link{pas_enhanceData}
+#'
+#' @examples
+#' \dontrun{
+#' example_pas_raw %>% 
+#'   pas_addSpatialMetadata() %>%  
+#'   pas_addAirDistrict() 
+#' }
+
+pas_addAirDistrict <- function(
+  pas = NULL
+) {
+  
+  # ----- Validate parameters --------------------------------------------------
+  
+  MazamaCoreUtils::stopIfNull(pas)
+  
+  if ( !pas_hasSpatial(pas) ) {
+    stop('Parameter `pas` does not contain required spatial metadata.
+          See `pas_addSpatialMetadata()` to add spatial meta data.')
+  }
+  
+  # ----- CARB Air Districts ---------------------------------------------------
+  
+  # NOTE: Currently (2020-04) only California Air basins is supported. 
+  
+  result <- try({ 
+    CA_AirBasins <- get(MazamaSpatialUtils::loadSpatialData("CA_AirBasins_01"))
+  }, silent = TRUE)
+  
+  if ( "try-error" %in% class(result) ) {
+    
+    logger.warn("Unable to load spatial data 'CA_AirBasins'.")
+    
+  } else {
+    
+    pas_CA <- 
+      pas %>% 
+      dplyr::filter(.data$countryCode == "US" & .data$stateCode == "CA")
+    
+    if ( nrow(pas_CA) > 0 ) {
+      
+      pas_CA$airDistrict <- 
+        MazamaSpatialUtils::getSpatialData(
+          pas_CA$longitude,
+          pas_CA$latitude,
+          CA_AirBasins,
+          useBuffering = TRUE
+        ) %>%
+        dplyr::pull("name")
+      
+      pas_nonCA <-  
+        pas %>% 
+        dplyr::filter(.data$countryCode != "US" | .data$stateCode != "CA")
+      
+      pas_nonCA$airDistrict <- as.character(NA)
+      
+      pas <- dplyr::bind_rows(pas_CA, pas_nonCA)
+      
+    }
+    
+  }
+  
+  # Add airDistrict if it hasn't already been added
+  if ( !"airDistrict" %in% names(pas) ) {
+    pas$airDistrict <- as.character(NA)
+  }
+    
+  return(pas)
+  
+}
---
+++ R/pas_addSpatialMetadata.R
@@ -0,0 +1,115 @@
+#' @importFrom MazamaCoreUtils logger.isInitialized logger.trace logger.warn logger.error logger.fatal
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @title Add Spatial Metadata to PurpleAIr Synoptic Data
+#'
+#' @description Adds spatial metadata to a pa_synoptic object via the 
+#' \code{MazamaSpatialUtils} Package using PurpleAir location coordinates to 
+#' determine country, state, and timezone.  
+#'
+#' @param pas PurpleAir Synoptic \emph{pas} object. 
+#' @param countryCodes (optional) ISO country codes used to subset the data.
+#'
+#' @return A pa_synoptic dataframe
+#' 
+#' @seealso \link{pas_enhanceData}
+#'
+#' @examples
+#' \dontrun{
+#' initializeMazamaSpatialUtils()
+#' pas_addSpatialMetadata(example_pas_raw)
+#' }
+
+pas_addSpatialMetadata <- function(
+  pas = NULL, 
+  countryCodes = NULL
+) {
+  
+  # TODO: Implement solid error handling, side-stepping the use of pas_isPas or 
+  # TODO: pas_isEmpty, as these require certain column vectors/names that may 
+  # TODO: not have been added to the (yet to be) pa_synoptic dataframe. Should order matter? 
+  
+  MazamaCoreUtils::stopIfNull(pas)
+  
+  # Ensure coordinate names are explicitly `longitude` and `latitude` 
+  colnames(pas)[stringr::str_which(names(pas), '^[lL]on')] <- 'longitude'
+  colnames(pas)[stringr::str_which(names(pas), '^[lL]at')] <- 'latitude'
+  
+  # Count rows to preform logging validation 
+  preValidationRows <- nrow(pas)
+  
+  # First, remove records with invalid locations
+  pas <-
+    pas %>%
+    dplyr::filter( !is.na(.data$longitude) & !is.na(.data$latitude) ) %>%
+    dplyr::filter( .data$longitude >= -180 & .data$longitude <= 180 ) %>%
+    dplyr::filter( .data$latitude >= -90 & .data$latitude <= 90 )
+  
+  badLocationCount <- preValidationRows - nrow(pas)
+  
+  if ( badLocationCount > 0 ) {
+    if ( logger.isInitialized() ) {
+      logger.trace("%d records removed because of invalid location data.", 
+                   badLocationCount)
+    }
+  }
+  
+  if ( logger.isInitialized() )
+    logger.trace("Adding spatial metadata")
+  
+  # TODO:  Could optimize spatial data assignment by only calculating spatial
+  # TODO:  data for the A channel and then copying that info to the B channel.
+  # TODO:  This will result in more complex code but would shave a few seconds
+  # TODO:  off of this step if that becomes critical.
+  
+  # Assign countryCodes
+  result <- try({
+    pas$countryCode <- MazamaSpatialUtils::getCountryCode(pas$longitude,
+                                                          pas$latitude,
+                                                          useBuffering = TRUE)
+  }, silent = TRUE)
+  
+  if ( "try-error" %in% class(result) ) {
+    err_msg <- geterrmessage()
+    if ( logger.isInitialized() ) {
+      logger.error(err_msg)
+      logger.fatal("Unable to assign countryCodes.")
+    }
+    stop(paste0("Unable to assign countryCodes: ", err_msg))
+  }
+  
+  # Suppress annoying 'deprecated' messages
+  suppressWarnings({
+    
+    if ( !is.null(countryCodes) ) {
+      
+      # Guarantee uppercase codes
+      countryCodes <- toupper(countryCodes)
+      
+      # Validate countryCodes
+      if ( any(!(countryCodes %in% countrycode::codelist$iso2c)) ) 
+        stop("parameter 'countryCodes' has values that are not recognized as ISO-2 country codes")
+      
+      #  Subset to countries of interest if country code provided
+      pas <- subset(pas, pas$countryCode %in% countryCodes)
+      
+    }
+    
+    # Assign stateCodes
+    pas$stateCode <- MazamaSpatialUtils::getStateCode(pas$longitude,
+                                                      pas$latitude,
+                                                      countryCodes = pas$countryCode,
+                                                      useBuffering = TRUE)
+    
+    # Assign timezones
+    pas$timezone <- MazamaSpatialUtils::getTimezone(pas$longitude,
+                                                    pas$latitude,
+                                                    countryCodes = pas$countryCode,
+                                                    useBuffering = TRUE)
+    
+  })
+  
+  return(pas)
+}
