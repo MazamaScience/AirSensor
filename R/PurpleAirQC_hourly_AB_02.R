25e21c1b07a829b0fef77242070007e9892a1675
Hans <hansmrtn@gmail.com>
2020-04-27 21:18:18 -0700

    init qc 02

---
+++ R/PurpleAirQC_hourly_AB_02.R
@@ -0,0 +1,67 @@
+#' @export
+#' @importFrom rlang .data
+#' 
+#' @title Apply QC to Aggregation Statistics
+#' 
+#' @param aggregationStats Dataframe of statistics as returned by
+#' \code{pat_aggregate()}.
+#' @param min_count Aggregation bins with fewer than `min_count` measurements
+#' will be marked as `NA`.
+#' @param returnAllColumns Logical specifying whether to return all columns
+#' of statistical data generated for QC algorithm or just the final `pm25` 
+#' result.
+#'  
+#' @description Creates a \code{pm25} timeseries by averaging aggregated data
+#' from the A and B channels and applying the following QC logic:
+#' 
+#' \enumerate{
+#' \item{Create pm25 by averaging the A and B channel aggregation means}
+#' \item{Invalidate data where:  (min_count < 20)}
+#' \item{Invalidate data where:  (p-value < 1e-4) & (mean_diff > 10)}
+#' \item{Invalidate data where:  (pm25 < 100) & (mean_diff > 20)}
+#' }
+#'
+#' @note Purple Air II sensors reporting after the June, 2019 firmware
+#' upgrade report data every 2 minutes or 30 measurements per hour. The default
+#' setting of \code{min_count = 20} is equivalent to a required data recovery
+#' rate of 67%.
+#' 
+#' @return Data frame with columns \code{datetime} and \code{pm25}.
+#' 
+#' @examples 
+#' \dontrun{
+#' }
+
+PurpleAirQC_hourly_AB_02 <- function(
+  pat = NULL, 
+  min_count = 20, 
+  threshold = 3,
+  max_diff = 0.5
+) { 
+  
+   percent_diff <- function(x,y) { abs(x-y)/((x+y)/2) }
+   
+   hour_mean <- pat_aggregate(pat, FUN = function(x) mean(x, na.rm = TRUE))
+   ch_mean <- rowMeans(cbind(hour_mean$data$pm25_A, hour_mean$data$pm25_B), na.rm = TRUE, dims = 1L)
+   bad_ch_diff <- percent_diff(hour_mean$data$pm25_A, hour_mean$data$pm25_B) > max_diff
+   
+   hour_count <- pat_aggregate(pat, FUN = function(x) length(na.omit(x)))$data
+   bad_min_count <- pmin(hour_count$pm25_A, hour_count$pm25_B, na.rm = TRUE) < min_count
+   
+   # mad 
+   hour_mad <- pat_aggregate(pat, FUN = function(x) mad(x, na.rm = TRUE))$data
+   bad_mad_diff <- percent_diff(hour_mad$pm25_A, hour_mad$pm25_B) > max_diff 
+   
+   bad_mad_A <- bad_mad_diff & hour_mad$pm25_A > threshold
+   bad_mad_B <- bad_mad_diff & hour_mad$pm25_B > threshold
+   # 
+   
+   hour_mean$data <- hour_mean$data %>% 
+     dplyr::mutate(pm25 = ch_pm25_avg) %>% 
+     dplyr::mutate(pm25 = replace(.data$pm25, bad_min_count, NA)) %>% 
+     dplyr::mutate(pm25 = replace(.data$pm25, bad_ch_diff, NA)) %>% 
+     dplyr::mutate(pm25 = replace(.data$pm25, bad_mad_A | bad_mad_B, NA))
+   
+   return(hour_mean)
+   
+}
