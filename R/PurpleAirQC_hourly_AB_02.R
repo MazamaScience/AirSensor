6a334af021b9362a18e494226f8bcc3ca9ad2633
Hans Martin <hansmrtn@gmail.com>
2020-04-30 20:02:11 -0700

    added and fixed docs

--- R/PurpleAirQC_hourly_AB_02.R
+++ R/PurpleAirQC_hourly_AB_02.R
@@ -4,10 +4,10 @@
 #' @title Apply QC to Aggregation Statistics
 #' 
 #' @param pat A PurpleAir timeseries object.
-#' @param min_count Aggregation bins with fewer than `min_count` measurements
-#' will be marked as `NA`.
-#' @param threshold A maximum median absolute deviation threshold.  
-#' @param max_diff A maximum percentage difference between pat channels (50% = 0.5). 
+#' @param min_count Aggregation bins with fewer than \code{min_count} measurements
+#' will be marked as NA.
+#' @param tolerance A maximum median absolute deviation threshold.  
+#' @param max_diff A maximum percentage difference between pat channels (50\% = 0.5). 
 #'  
 #' @description Creates a \code{pm25} timeseries by averaging aggregated data
 #' from the A and B channels, calculates MAD within the threshold, and values 
@@ -16,7 +16,7 @@
 #' @note Purple Air II sensors reporting after the June, 2019 firmware
 #' upgrade report data every 2 minutes or 30 measurements per hour. The default
 #' setting of \code{min_count = 20} is equivalent to a required data recovery
-#' rate of 67%.
+#' rate of 67\%.
 #' 
 #' @return Data frame with columns \code{datetime} and \code{pm25}.
 #' 
@@ -26,33 +26,42 @@
 PurpleAirQC_hourly_AB_02 <- function(
   pat = NULL, 
   min_count = 20, 
-  threshold = 3,
+  tolerance = 3,
   max_diff = 0.5
 ) { 
   
+   # Create function to calculate the percent difference between chA and chB
    percent_diff <- function(x,y) { abs(x-y)/((x+y)/2) }
    
-   hour_mean <- pat_aggregate(pat, FUN = function(x) mean(x, na.rm = TRUE))
-   ch_mean <- rowMeans(cbind(hour_mean$data$pm25_A, hour_mean$data$pm25_B), na.rm = TRUE, dims = 1L)
-   bad_ch_diff <- percent_diff(hour_mean$data$pm25_A, hour_mean$data$pm25_B) > max_diff
+   # Aggregate hourly pat mean
+   patMean <- pat_aggregate(pat, FUN = function(x) mean(x, na.rm = TRUE))
    
-   hour_count <- pat_aggregate(pat, FUN = function(x) length(na.omit(x)))$data
-   bad_min_count <- pmin(hour_count$pm25_A, hour_count$pm25_B, na.rm = TRUE) < min_count
+   # Logical - Flag exceeded channel differences  
+   chDiffMask <- percent_diff(patMean$data$pm25_A, patMean$data$pm25_B) > max_diff
    
-   # mad 
-   hour_mad <- pat_aggregate(pat, FUN = function(x) stats::mad(x, na.rm = TRUE))$data
-   bad_mad_diff <- percent_diff(hour_mad$pm25_A, hour_mad$pm25_B) > max_diff 
+   # Calculate the mean of chA and chB
+   chMean <- rowMeans(cbind(patMean$data$pm25_A, patMean$data$pm25_B), na.rm = TRUE, dims = 1L)
    
-   bad_mad_A <- bad_mad_diff & hour_mad$pm25_A > threshold
-   bad_mad_B <- bad_mad_diff & hour_mad$pm25_B > threshold
-   # 
+   # Aggregate hourly pat to NA omitted vector length
+   patCount <- pat_aggregate(pat, FUN = function(x) length(na.omit(x)))$data
    
-   hour_mean$data <- hour_mean$data %>% 
-     dplyr::mutate(pm25 = ch_mean) %>% 
-     dplyr::mutate(pm25 = replace(.data$pm25, bad_min_count, NA)) %>% 
-     dplyr::mutate(pm25 = replace(.data$pm25, bad_ch_diff, NA)) %>% 
-     dplyr::mutate(pm25 = replace(.data$pm25, bad_mad_A | bad_mad_B, NA))
+   # Logical - Flag under minimum counts
+   minCountMask <- pmin(patCount$pm25_A, patCount$pm25_B, na.rm = TRUE) < min_count
    
-   return(hour_mean)
+   # Aggregate hourly pat median absolute deviation
+   patMAD <- pat_aggregate(pat, FUN = function(x) stats::mad(x, na.rm = TRUE))$data
+   
+   # Logical - Flag out-of-tolerance MAD
+   chA_MADMask <- patMAD$pm25_A > tolerance
+   chB_MADMask <- patMAD$pm25_B > tolerance
+   
+   # Replace any masked values with NA, create new column `pm25` of channel mean
+   patMean$data <- patMean$data %>% 
+     dplyr::mutate(pm25 = chMean) %>% 
+     dplyr::mutate(pm25 = replace(.data$pm25, minCountMask, NA)) %>% 
+     dplyr::mutate(pm25 = replace(.data$pm25, chDiffMask, NA)) %>% 
+     dplyr::mutate(pm25 = replace(.data$pm25, chA_MADMask | chB_MADMask, NA))
+   
+   return(patMean)
    
 }
