a5c62a413dfc67b9fcfba319418a53b17c86628c
Hans Martin <hansmrtn@gmail.com>
2020-05-09 10:15:10 -0700

    refactored to s3 methods

--- R/pat_aggregate.R
+++ R/pat_aggregate.R
@@ -3,47 +3,25 @@
 #' @importFrom rlang .data
 #' @importFrom stats aggregate median na.omit quantile sd t.test time
 #'
-#' @title Aggregate PurpleAir Timeseries objects
+#' @title Aggregate PurpleAir Timeseries Data
 #' 
 #' @param pat PurpleAir Timeseries \emph{pat} object.
 #' @param FUN The function to be applied to each vector of numeric \code{pat} data.
 #' @param unit Character string specifying temporal units for binning.
 #' @param count Number of units per bin.
-#' @param level Context level to evaluate FUN on. 
-#' 
-#' @description Aggregate PurpleAir timeseries (\emph{pat}) objects along its 
+#'
+#' @description Aggregate PurpleAir timeseries (\emph{pat}) objects along its
 #' datetime axis. Temporal aggregation involves splitting a \emph{pat} object into
-#' separate bins along its datetime axis, applying a function to the split data, 
-#' and then recombining the data along a new aggregate datetime axis.
-#' 
-#' The recommended and default level (\code{level = 2}) 
-#' \code{FUN} is mapped to the \code{pat} _numeric_ variables in each bin, 
-#' which are then recombined into an aggregated \emph{pat} object containing 
-#' the same metadata as the incoming \code{pat}. The returned object is 
-#' effectively a \emph{pa_timeseries} object that will behave similarly to 
-#' un-aggregated \emph{pat} objects throughout the package. \code{level = 2} is 
-#' the recommended context level to operate on if preforming quick vectorised 
-#' numeric operations (e.g. \code{mean}, \code{min}, \code{var}, etc.). 
-#' 
-#' To operate on the incoming \code{pat} data on a self-object _level_, set \code{level = 1}. 
-#' This more advanced option is provided to allow explicit \emph{pa_timeseries} 
-#' object data manipulation while aggregating. Setting \code{level = 1} allows 
-#' \code{FUN} to access and operate on all numeric \code{pat} data. Setting \code{level = 1} requires 
-#' explicit function calls and return operations, and therefore will always 
-#' \code{FUN} execution call with an attached aggregate datetime axis, 
-#'  _not_ a \emph{pa_timeseries} object. 
-#' 
-#' @details \code{FUN} must operate on univariate numeric vectors and return a 
-#' scalar value. Besides the data variable, no additional arguments will be 
+#' seperate bins along its datetime axis. \code{FUN} is mapped to the \emph{pat}
+#' numeric variables in each bin, which are then recombined into an aggregated
+#' \emph{pat} object containing the same metadata as the incomting \code{pat}.
+#'
+#' @details \code{FUN} must operate on univariate numeric vectors and return a
+#' scalar value. Besides the data variable, no additional arguments will be
 #' provided to this function. This means that functions like \code{mean} and
-#' \code{max} will need to be wrapped in a function that specifies 
+#' \code{max} will need to be wrapped in a function that specifies
 #' \code{na.rm = TRUE}. See the examples below.
-#' 
-#' \code{level} specifies the context level for \code{FUN} to operate on. To 
-#' aggregate \code{pat} data where \code{FUN} must operate of access multiple 
-#' columns (unvectorised) use \code{level = 1}. Otherwise, use vecotrised method, 
-#' \code{level = 2}.
-#' 
+#'
 #' @return Returns an aggregated \emph{pat} object.
 #' 
 #' @examples
@@ -85,27 +63,34 @@
 #'   dplyr::slice(1:6)
 #'   
 #' # Two Sample Student T-Test (advanced users only - see details.)
-#' FUN_ttest <- function(x) {
-#'   t.test(x$pm25_A, x$pm25_B)
+#' FUN_ttest <- function(data) {
+#'   t.test(data$pm25_A, data$pm25_B)
 #' }
 #' 
-#' pat %>% 
-#'   pat_aggregate(FUN_ttest, level = 1) 
-#'   
+#' pat %>%
+#'   pat_extractData() %>%
+#'   pat_aggregate(FUN_ttest)
+#'
+pat_aggregate <- function(pat, FUN, unit, count) {
+  UseMethod('pat_aggregate', pat)
+}
 
-pat_aggregate <- function(
+#' @export
+#' @describeIn pat_aggregate The default S3 method for \emph{pa_timeseries} objects.
+pat_aggregate.pa_timeseries <- function(
   pat, 
   FUN = function(x) { mean(x, na.rm = TRUE) }, 
   unit = "minutes",
-  count = 60, 
-  level = 2
+  count = 60
 ) {
   
   # ----- Validate parameters --------------------------------------------------
-  
+
   MazamaCoreUtils::stopIfNull(pat)
   MazamaCoreUtils::stopIfNull(FUN)
-  
+  MazamaCoreUtils::stopIfNull(unit)
+  MazamaCoreUtils::stopIfNull(count)
+
   if ( !pat_isPat(pat) )
     stop("Parameter 'pat' is not a valid 'pa_timeseries' object.")
   
@@ -155,26 +140,7 @@ pat_aggregate <- function(
   # NOTE: level = 2 is operate vectorised data (each col of patData) individually.
   # NOTE:  See ?Map and also ?mapply to understand the mapping below.
 
-  if ( level == 1L ) { # Advanced 
-    
-    # Map the function FUN to each bin data.frame. 
-    mapped <- base::Map(
-      patData_bins,
-      f = function(patData, f = FUN) { f(patData) }
-    )
-    
-    # Return a data.frame of aggregate data
-    aggData <- data.frame(
-      'datetime' = datetime,
-      do.call(rbind, mapped),
-      fix.empty.names = FALSE,
-      check.rows = FALSE,
-      check.names = FALSE
-      )
-    
-    return(aggData)
-    
-  } else if ( level == 2L ) { # Default
+
     
     # Map each binned hourly data.frame to the user defined lambda-like 
     # function f applied via apply to each vector in the mapped data.frame
@@ -192,11 +158,72 @@ pat_aggregate <- function(
     )
     
     return(pat)
-    
-  } else {
-    
-    stop('Invalid level for FUN.')
-    
-  }
 
+}
+#' @export
+#' @describeIn pat_aggregate  Recommended for advanced users only.
+#' Evaluate \code{FUN} on a data.frame that contain a valid 'datetime' column.
+#' Returns a data.frame object.
+pat_aggregate.data.frame <- function(
+  df,
+  FUN = function(df) { mean(df$pm25_A + df$pm25_B, na.rm = TRUE) },
+  unit = 'minutes',
+  count = 60
+) {
+
+  MazamaCoreUtils::stopIfNull(df)
+  MazamaCoreUtils::stopIfNull(FUN)
+  MazamaCoreUtils::stopIfNull(unit)
+  MazamaCoreUtils::stopIfNull(count)
+
+  # ----- Aggregate Data -------------------------------------------------------
+
+  # Only use numeric columns for aggregation matrix
+  numeric_cols <- which(unlist(lapply(df, is.numeric)))
+
+  # Convert to eXtensible Time Series (xts) data.frame
+  # Separate only useful data for calculation (i.e. only numeric)
+  df <- xts::xts(
+    x = df[numeric_cols],
+    order.by = df$datetime,
+    unique = TRUE,
+    tzone = 'UTC'
+  )
+
+  # Split the xts into a list of binned xts matrices
+  df_bins <- xts::split.xts(
+    df,
+    f = unit,
+    drop = FALSE,
+    k = count
+  )
+
+  # Get the first index of aligned time for future use.
+  datetime <- as.numeric(
+    lapply(
+      X = df_bins,
+      # Select first datetime index in bin to use as aggregated datetime axis
+      FUN = function(x) zoo::index(x)[1] ## First # [nrow(x)] ## Last
+    )
+  )
+  # Convert saved datetime vector back to posix* from int
+  class(datetime) <- c("POSIXct", "POSIXt")
+  attr(datetime, 'tzone') <- 'UTC'
+
+  # Map the function FUN to each bin data.frame.
+  mapped <- base::Map(
+    df_bins,
+    f = function(df, f = FUN) { f(df) }
+  )
+
+  # Return a data.frame of aggregate data
+  aggData <- data.frame(
+    'datetime' = datetime,
+    do.call(rbind, mapped),
+    fix.empty.names = FALSE,
+    check.rows = FALSE,
+    check.names = FALSE
+  )
+
+  return(aggData)
 }
